d3de1fc767fb5dda5f1728d6a26048d42d8e5fbb
Chris Farrugia <foxcode9@gmail.com>
2019-09-13 16:04:30 +0200

    Update app2.py

--- Udemy/The Ultimate Flask Course/1. Flask Basics/app2.py
+++ Udemy/The Ultimate Flask Course/1. Flask Basics/app2.py
@@ -69,6 +69,76 @@ def form_out():
 
 #-----------------------------------------------------------
 
+# getting values back from a json object instead of an html form.
+# for this i am using postman app.
+
+from flask import jsonify
+from random import choice
+
+# in postman i have selected raw > JSON(application/json) and input:
+# {"name":"Sarah", "location":"Malta","randomlist":["one","two","three","four"]}
+
+@app.route('/processjson', methods=['POST', 'GET'])
+def processjson():
+    
+    data = request.get_json() 
+
+    name = data['name']
+    location = data['location']
+    randomlist = data['randomlist']
+
+    return jsonify({
+        'result' : 'Success!', 
+        'name' : name,
+        'location' : location,
+        'randomkeyinlist' : choice(randomlist)})
+
+
+# result :  {
+#   "location": "Malta",
+#   "name": "Sarah",
+#   "randomkeyinlist": "two",
+#   "result": "Success!"
+# }
+
+#-----------------------------------------------------------
+
+# getting the method use for request: 
+
+# here we are combining both 'GET' and 'POST' methode in one route
+# and then we are process them differently depending on the method
+# that was used 
+
+@app.route('/case-ref', methods=['GET','POST'])
+def caseref():
+
+    if request.method == 'GET':
+        return'''
+<body>
+    <form action="case-ref" method="POST">
+        <p>
+            <select name="prefix" id="prefix">
+                <option value="PA">PA</option>
+                <option value="DN">DN</option>
+                <option value="RC">RC</option>
+            </select>
+            <input type="number" name="number" max="99999">
+            <input type="number" name="year" max="99">
+            <input type="submit">
+        </p>
+    </form>
+</body>       
+        '''
+
+    else:
+        prefix = request.form['prefix']
+        number = request.form['number']
+        year = request.form['year'] 
+
+        return f'<h2>Case Number: {prefix}{number}-{year}</h2>'  
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
