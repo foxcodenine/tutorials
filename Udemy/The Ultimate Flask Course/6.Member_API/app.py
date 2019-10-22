923b7f4077ca9ca3a5f200d113eaae5a8bd824d7
Chris Farrugia <foxcode9@gmail.com>
2019-10-22 11:07:28 +0200

    Add files via upload

--- Udemy/The Ultimate Flask Course/6.Member_API/app.py
+++ Udemy/The Ultimate Flask Course/6.Member_API/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, request, jsonify
 import sqlite3
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -12,6 +13,8 @@ def connect_db():
     sql.row_factory = sqlite3.Row 
     return sql
 
+
+
 def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db() 
@@ -23,10 +26,27 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+#_______________________________________________________________________
+
+
+api_username = 'admin'
+api_password = 'password'
+
+def protected(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth   and   auth.username == api_username   and   auth.password == api_password:
+            return f(*args, **kwargs)
+        return jsonify({'message' : 'Authentication Failed!'}), 403 
+    return decorated
+
+
 
 #_______________________________________________________________________
 
 @app.route('/member', methods=['GET'])
+@protected
 def get_members():
 
     db = get_db()
@@ -39,21 +59,35 @@ def get_members():
         member = dict(m)
         return_values.append(member)
 
+    
     return jsonify({'members' : return_values})
 
+    
 
+# ________________________________________
 
 @app.route('/member/<int:member_id>', methods=['GET'])
+@protected
 def get_member(member_id):
-    return 'This return one member by ID'
 
+    db = get_db()
+    member_cur = db.execute("SELECT * FROM members WHERE id=?", [member_id])
+    member_result = member_cur.fetchone()
+
+    member_result = dict(member_result)
+
+
+    return jsonify({'member':  member_result})
+
+# ________________________________________
 
 @app.route('/member', methods=['POST'])
+@protected
 def add_member():
     
     
-
     new_member_data = request.get_json()
+
     name = new_member_data['name']
     email = new_member_data['email']
     level = new_member_data['level']
@@ -66,18 +100,45 @@ def add_member():
     new_member = member_cur.fetchone()
     new_member = dict(new_member)
 
-    return jsonify(new_member)
+    return jsonify({'member': new_member})
 
+# ________________________________________
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
+@protected
 def edit_member(member_id):
-    return 'This updates a member by ID.'
 
+    new_member_data = request.get_json() 
+
+    name = new_member_data['name']
+    email = new_member_data['email']
+    level = new_member_data['level']
+
+    db = get_db()
+    db.execute("UPDATE members SET name=?, email=?, level=? WHERE id=?", [name, email, level, member_id])
+    db.commit
+
+    member_cur = db.execute("SELECT * FROM members WHERE id=?", [member_id])
+    member_result = member_cur.fetchone()
+
+    member_result = dict(member_result)
+
+    return jsonify({'member' : member_result})
+
+# ________________________________________
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
+@protected
 def delete_member(member_id): 
-    return 'This removes a member by ID.'
+
+    db = get_db()
+    db.execute("DELETE FROM members WHERE id=?",[member_id])
+    db.commit()
+
+    return jsonify({"message" : 'The member has been deleted!'})
     
+#_______________________________________________________________________
  
+
 if __name__ == "__main__":
     app.run()
