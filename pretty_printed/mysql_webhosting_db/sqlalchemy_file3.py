728dffaf5e422a605bd2eb314c8cae6a359cfc55
Christopher Farrugia <foxcode9@gmail.com>
2019-10-28 20:27:25 +0100

    Update sqlalchemy_file3.py

--- pretty_printed/mysql_webhosting_db/sqlalchemy_file3.py
+++ pretty_printed/mysql_webhosting_db/sqlalchemy_file3.py
@@ -8,7 +8,7 @@ from sqlalchemy_file2 import Person, Pet
 
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://F2udETTsO6:U7hQzPXXBW@remotemysql.com/F2udETTsO6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://F2udETTsO6:I7hQzPXXBW@remotemysql.com/F2udETTsO6'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True 
 
