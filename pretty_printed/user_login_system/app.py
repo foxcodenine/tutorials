ae7384f4b686c6735ebb7b3e65d83e878fc9c117
Christopher Farrugia <foxcode9@gmail.com>
2019-11-21 09:01:34 +0100

    Add files via upload

--- pretty_printed/user_login_system/app.py
+++ pretty_printed/user_login_system/app.py
@@ -1,29 +1,99 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField 
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
-Bootstrap(app) 
+
 
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'ThisIsASecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://oOp9iEOuPn:SAEm6rAKiE@remotemysql.com/oOp9iEOuPn'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+Bootstrap(app) 
+
+db = SQLAlchemy(app)
+
+
+# ______________________________________________________________________
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+# _____________________
+
+# db.create_all()
+
+
+# ______________________________________________________________________
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(),  Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+# ______________________________________________________________________
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# _____________________
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+        
+
+    return render_template('login.html', form=form)
+
+# _____________________
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = RegisterForm() 
+
+    if form.validate_on_submit():
+        
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h4>New user has been created!</h4>'
+
+        # return '<h2>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h2>'
+
+
+    return render_template('signup.html', form=form)
+
+
+# _____________________
 
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+
+# ______________________________________________________________________
 
 
 if __name__ == '__main__':
