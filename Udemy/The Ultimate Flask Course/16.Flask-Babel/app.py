20c883c4121fafd472713b2172e4f2ba971b8926
Chris Farrugia <foxcode9@gmail.com>
2019-12-29 20:35:25 +0100

    Add files via upload

---
+++ Udemy/The Ultimate Flask Course/16.Flask-Babel/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for, redirect, request 
+from flask_babel import Babel, get_locale
+
+# ______________________________________________________________________
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+babel = Babel(app) 
+# ______________________________________________________________________
+
+@babel.localeselector
+def localeselector():
+    # return 'en_UK'
+    return request.accept_languages.best_match(['en', 'es','de','ru','mt'])
+
+
+
+@app.route('/')
+def index():
+    return '<h2>Locale: {}</h2>'.format(get_locale())
+
+
+# ______________________________________________________________________
+
+if __name__ == '__main__':
+    app.run()
