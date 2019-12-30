03e2fa8b1c2812ab85304409d579b9b6ef6c1b0c
Christopher Farrugia <54644062+foxcodenine@users.noreply.github.com>
2019-12-30 22:29:20 +0100

    Add files via upload

---
+++ Udemy/The Ultimate Flask Course/16.Flask-Babel/app3.py
@@ -0,0 +1,38 @@
+# https://pythonhosted.org/Flask-Babel/
+# ______________________________________________________________________
+
+from flask import Flask, render_template, url_for, redirect, request 
+from flask_babel import Babel, get_locale, format_date, format_datetime 
+from datetime import date, datetime
+
+# ______________________________________________________________________
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+babel = Babel(app) 
+# ______________________________________________________________________
+
+@babel.localeselector
+def localeselector():
+    # return 'en_UK'
+    return request.accept_languages.best_match(['en_US','en_GB', 'es_ES'])
+
+
+
+@app.route('/')
+def index():
+    d = date(2011, 1, 8)
+    dt = datetime(2010, 4, 9, 21, 13)
+
+    local_date = format_date(d)
+    local_datetime = format_datetime(dt)
+
+    return render_template('index.html', Local=get_locale(), Local_date=local_date, Local_datetime=local_datetime)
+
+
+# ______________________________________________________________________
+
+if __name__ == '__main__':
+    app.run()
