5693729054876fc8a55d861b948eeb434b3da79f
Christopher Farrugia <foxcode9@gmail.com>
2020-01-07 11:46:38 +0100

    Add files via upload

--- Udemy/The Ultimate Flask Course/17.Flask-restless/app.py
+++ Udemy/The Ultimate Flask Course/17.Flask-restless/app.py
@@ -1,3 +1,9 @@
+
+# https://flask-restless.readthedocs.io/en/latest/
+# https://flask-restless.readthedocs.io/en/stable/
+
+# ______________________________________________________________________
+
 from flask import Flask, render_template, redirect, url_for, request 
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
@@ -37,8 +43,68 @@ class Item(db.Model):
 
 # ______________________________________________________________________
 
-manager.create_api(User, methods=['GET', 'POST'])
-manager.create_api(Item, methods=['GET', 'POST'])
+manager.create_api(User, methods=['GET', 'POST', 'PUT'])
+manager.create_api(Item, methods=['GET', 'POST', 'DELETE','PUT'])
+
+
+"""
+How to query to an api in Postmen:
+In these examples we are using the item table:
+
+to LIMIT:
+
+http://127.0.0.1:5000/api/item?q={"limit":3}
+
+to OFFSET
+
+http://127.0.0.1:5000/api/item?q={offset":2}
+http://127.0.0.1:5000/api/item?q={"limit":3, "offset":2}
+
+to ORDER_BY
+
+http://127.0.0.1:5000/api/item?q={"order_by":[{"field":"id", "direction":"asc"}]}
+http://127.0.0.1:5000/api/item?q={"order_by":[{"field":"id", "direction":"desc"}]}
+http://127.0.0.1:5000/api/item?q={"order_by":[{"field":"id", "direction":"desc"}],"limit":3, "offset":2}
+
+
+SEARCH QUERY
+
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": "==", "val": 1}]}
+
+double query:
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": ">", "val": 2},{"name": "id","op": "<", "val": 7}]}
+
+
+name = fieldname / column
+op = operator :
+                ==, eq, equals, equals_to
+                !=, neq, does_not_equal, not_equal_to
+                >, gt, <, lt
+                >=, ge, gte, geq, <=, le, lte, leq
+                in, not_in
+                is_null, is_not_null
+                like
+                has
+                any
+val = value
+
+
+
+/* in, not_in */
+
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": "in", "val": [1, 3, 7]}]}
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": "not_in", "val": [1, 3, 7]}]}
+
+
+/* is_null, is_not_null */
+
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "user_id","op": "is_null"}]}
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "user_id","op": "is_not_null"}]}
+
+/* like */
+http://127.0.0.1:5000/api/user?q={"filters":[{"name": "name","op": "like", "val": "chr%"}]}
+
+"""
 
 # ______________________________________________________________________
 
