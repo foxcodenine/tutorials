5063cd8edac939e0553b37675eab9efc1564f7a3
Christopher Farrugia <54644062+foxcodenine@users.noreply.github.com>
2020-01-09 09:34:51 +0100

    Add files via upload

--- Udemy/The Ultimate Flask Course/17.Flask-restless/app.py
+++ Udemy/The Ultimate Flask Course/17.Flask-restless/app.py
@@ -44,10 +44,13 @@ class Item(db.Model):
 # ______________________________________________________________________
 
 manager.create_api(User, methods=['GET', 'POST', 'PUT'])
-manager.create_api(Item, methods=['GET', 'POST', 'DELETE','PUT'])
+manager.create_api(Item, methods=['GET', 'POST', 'DELETE','PATCH'], allow_delete_many=True, allow_patch_many=True)
 
 
 """
+
+https://flask-restless.readthedocs.io/en/stable/requestformat.html#pagination
+
 How to query to an api in Postmen:
 In these examples we are using the item table:
 
@@ -71,7 +74,7 @@ SEARCH QUERY
 
 http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": "==", "val": 1}]}
 
-double query:
+double query (AND):
 http://127.0.0.1:5000/api/item?q={"filters":[{"name": "id","op": ">", "val": 2},{"name": "id","op": "<", "val": 7}]}
 
 
@@ -103,6 +106,47 @@ http://127.0.0.1:5000/api/item?q={"filters":[{"name": "user_id","op": "is_not_nu
 
 /* like */
 http://127.0.0.1:5000/api/user?q={"filters":[{"name": "name","op": "like", "val": "chr%"}]}
+
+
+/* or */ 
+http://127.0.0.1:5000/api/item?q={"filters":[{"or":[{"name": "id","op": "==", "val": 3},{"name": "id","op": "==", "val": 5}]}]}
+
+
+/* any */   (from  parent to child )
+http://127.0.0.1:5000/api/user?q={"filters":[{"name": "items","op": "any", "val": {"name": "id","op": ">=", "val": 1}}]}
+
+
+/* has */   (from child to parent)
+http://127.0.0.1:5000/api/item?q={"filters":[{"name": "user","op": "has", "val": {"name": "id","op": ">", "val": 1}}]}
+
+
+/* Delete */ 
+need to set     allow_delete_many=True    in the manager.create_api
+select the Delete method  and  send
+
+http://127.0.0.1:5000/api/item?q={"filters":[{"name":"id","op":">","val":6}]}
+
+
+
+/* Put Patch*/ 
+
+
+need to set     allow_patch_many=True    in the manager.create_api
+select the PATCH method  and  send
+
+http://127.0.0.1:5000/api/item?q={"filters":[{"name":"id","op":">","val":4}]}
+
+There is a bug allow_patch_many=True that it pathch everything not just the selected.       
+
+
+
+Pagination
+
+to set the page number do
+http://127.0.0.1:5000/api/item?page=2
+
+to set the results per page do
+http://127.0.0.1:5000/api/item?page=2&results_per_page=3
 
 """
 
