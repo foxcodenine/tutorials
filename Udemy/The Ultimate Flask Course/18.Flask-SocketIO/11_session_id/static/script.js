4d1f773ea9a418d0379fc79d9dc273152b47fe44
Christopher Farrugia <54644062+foxcodenine@users.noreply.github.com>
2020-01-15 21:08:33 +0100

    Add files via upload

---
+++ Udemy/The Ultimate Flask Course/18.Flask-SocketIO/11_session_id/static/script.js
@@ -0,0 +1,78 @@
+// alert('PAGE WORK')
+
+
+$(document).ready(function(){
+
+
+    var scoket = io.connect('http://127.0.0.1:5000')
+
+    //--------------------------
+
+    $('#send').on('click', function(){
+
+        var message = $('#message').val();
+
+        scoket.emit('message from user', message);        
+    });
+
+    //--------------------------
+
+    scoket.on('forward from flask', function(msg){
+        alert(msg);
+    });
+
+    //--------------------------
+
+    scoket.on('server_originated', function(msg){
+        alert(msg);
+    });
+
+    //-------------------------
+
+    // Session_id
+
+    var private_scoket = io.connect('http://127.0.0.1:5000/private')
+
+    $('#send_username').on('click', function(){
+
+        var username = $('#username').val();
+
+        private_scoket.emit('username', username);
+
+    });
+    //--------------------------
+
+
+// //___________________________________________________________________
+    // /* Here we are listening to the connect event and when it happens we are sending
+    // a message from the client to server  */
+    // scoket.on('connect', function() {
+    //     scoket.send('I am now connected!');  
+        
+
+        // /* Here we are listening to the server message (C) and sending it as an
+        // alert */
+        // scoket.on('message', function(msg) {
+        //     alert(msg);
+        // });
+        
+
+        // /* emit vs send.  emit is a message with a name */
+        // // scoket.emit('custom event', 'The custom event message!');
+        // scoket.emit('custom event', {'name' : 'Anthony'});
+
+        // // scoket.on('from flask', function(msg) {
+        // //     alert(msg);
+        // // });
+
+
+        // /* Here we are listening to the emit server message (CC) and sending it as an
+        // alert */
+        // scoket.on('from flask', function(msg) {
+        //     alert(msg['extension']);
+        // });       
+
+    // }); 
+// //___________________________________________________________________
+
+});
