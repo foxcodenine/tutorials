3055d32fe8d2cc8e0c816b51a0538f283a3e83b5
foxcodenine <foxcode9@gmail.com>
2020-02-01 10:13:11 +0100

    update to a2_defining_schema_SQLAlchemy_core.py

--- sqlalchemy-101/a2_defining_schema_SQLAlchemy_core.py
+++ sqlalchemy-101/a2_defining_schema_SQLAlchemy_core.py
@@ -9,6 +9,8 @@ from sqlalchemy import create_engine
 from sqlalchemy import MetaData, Table, Column
 from sqlalchemy import String, Text, DateTime, Boolean, Integer
 from sqlalchemy import ForeignKey
+from sqlalchemy import PrimaryKeyConstraint, ForeignKeyConstraint, \
+    UniqueConstraint, CheckConstraint
 
 from datetime import datetime
 
@@ -66,7 +68,7 @@ employees = Table('employees', metadata,
 )
 
 employees_detairs = Table('employee_details', metadata,
-    Column('employee_id', ForeignKey('employees. employee_id'), primary_key=True),
+    Column('employee_id', ForeignKey('employees.employee_id'), primary_key=True),
     Column('ssn', String(200), nullable=False),
     Column('salary', String(200), nullable=False),
     Column('blood_group', String(200), nullable=False),
@@ -79,5 +81,132 @@ employees_detairs = Table('employee_details', metadata,
 # ________________________________
 # Many-to-Many relationship
 
+postcodes = Table('postcodes', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('post_title', String(200), nullable=False),
+    Column('post_slug', String(200), nullable=False),
+    Column('content', Text(), nullable=False)    
+)
 
-# metadata.create_all(engine)
+tages = Table('tages', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('tag', String(200), nullable=False),
+    Column('tag_slag', String(200), nullable=False)
+)
+
+postcode_tage = Table('postcodes_tage', metadata,
+    Column('postcodes_id', Integer(), ForeignKey('postcodes.id')),
+    Column('tage_id', Integer(), ForeignKey('tages.id'))
+)
+
+
+
+
+# ______________________________________________________________________
+
+# Defining Constraint at the Table level
+
+# PrimaryKeyConstraint
+# ForeignKeyConstraint
+# UniqueConstraint
+# CheckConstraint
+# Index
+
+# ____________________________
+# Adding Primary Key Constraint using PrimaryKeyConstraint
+
+# parents =Table('parents', metadata,
+#     Column('acc_no', Integer()),
+#     Column('acc_type', Integer(), nullable=False), 
+#     Column('name', String(20), nullable=False), 
+#     PrimaryKeyConstraint('acc_no', name='acc_no_pk')
+# )
+
+# Mainly, the PrimaryKeyConstraint is used to define a composite primary
+# key (a primary key spanning more than one columns). For example:
+
+parents =Table('parents', metadata,
+    Column('acc_no', Integer()),
+    Column('acc_type', Integer(), nullable=False), 
+    Column('name', String(20), nullable=False), 
+    PrimaryKeyConstraint('acc_no', 'acc_type', name='acc_no_pk')
+)
+
+# ____________________________
+# Creating Foreign Key using ForeignKeyConstraint
+
+
+# genitori = Table('genitori', metadata, 
+#     Column('id', Integer(), primary_key=True), 
+#     Column('name', String(50), nullable=False)
+# )
+
+# figlio = Table('figlio', metadata, 
+#     Column('id', Integer(), primary_key=True), 
+#     Column('genitore_id', Integer(), nullable=False), 
+#     Column('name', String(50), nullable=False), 
+#     ForeignKeyConstraint(['genitore_id'], ['genitori.id'])    
+# )
+
+# The real utility of ForeignKeyConstraint comes into play when you want
+# to define composite foreign key (a foreign key which consists of more
+# than one columns). For example:
+
+genitori = Table('genitori', metadata, 
+    Column('id', Integer()),
+    Column('ssn', Integer()),
+    Column('name', String(50), nullable=False),
+    PrimaryKeyConstraint('id','ssn', name='uniq_1')
+)
+
+
+figlio = Table('figlio', metadata, 
+    Column('id', Integer(), primary_key=True), 
+    Column('genitore_id', Integer(), nullable=False), 
+    Column('genitore_ssn', Integer(),nullable=False),
+    Column('name', String(50), nullable=False), 
+    ForeignKeyConstraint(['genitore_id', 'genitore_ssn'], 
+                         ['genitori.id', 'genitori.ssn'])    
+)
+
+# _________________________________
+
+# Creating Unique Constraint using UniqueConstraint
+
+
+
+retailers = Table('retailers', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('vat_no', Integer(), nullable=False),
+    Column('name', String(255), nullable=False), 
+    UniqueConstraint('vat_no', name='unique_vat_no')
+)
+
+# The UniqueConstraint is commonly used to define unique constraint
+# containing multiple columns. For example:
+
+
+boutiques = Table('boutiques', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('acc_no', Integer(), nullable=False),
+    Column('vat_no', Integer(), nullable=False),
+    Column('name', String(255), nullable=False), 
+    UniqueConstraint('acc_no','vat_no', name='uniq_1')
+)
+
+
+# _________________________________
+
+# We can add CHECK constraint using the CheckConstraint construct.
+operators = Table('operators', metadata, 
+    Column('id', Integer(), primary_key=True), 
+    Column('name', String(50), nullable=False), 
+    Column('salary', Integer(), nullable=False),
+    CheckConstraint('salary >= 1000', name='salary_check')
+)
+# Currently not supported by mysql
+
+
+
+
+metadata.create_all(engine)
