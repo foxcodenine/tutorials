6b236d0b56f80409b4adc296501e5adf589862d4
Chris Farrugia <foxcode9@gmail.com>
2020-02-03 20:55:32 +0100

    update a3_e-commerce_app.py

--- sqlalchemy-101/a3_e-commerce_app.py
+++ sqlalchemy-101/a3_e-commerce_app.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine, Table, Column, String, Integer, MetaData
 from sqlalchemy import DateTime, ForeignKey, Numeric, CheckConstraint, DefaultClause
+from sqlalchemy import insert
 from sqlalchemy.sql import func, text
 from datetime import datetime
 
+def pl():
+    print('\n_______________________\n')
 
-engine = create_engine('mysql+pymysql://root:*****@localhost/e_commerce')
+
+engine = create_engine('mysql+pymysql://root:***@localhost/e_commerce')
 metadata = MetaData(engine)
 
 customers = Table('customers', metadata,
@@ -47,5 +51,117 @@ order_lines = Table('order_lines', metadata,
     CheckConstraint('quantity > 0')
 )
 
+# ______________________________________________________________________
+
+# Inserting Records
+
+# 1st method:
+conn = engine.connect()
+
+ins = customers.insert().values(
+    first_name = 'John',
+    last_name = 'Green',
+    username = 'johngreen',
+    email = 'johngreen@mail.com',
+    address = '164 Hidden Valley Road',
+    town = 'Norfolk'
+)
+
+
+r = conn.execute(ins)
+
+
+
+# ______________________
+pl()
+# To view the SQL this code would generate type the following:
+print(str(ins))
+pl()
+# To  view the values:
+print(ins.compile().params)
+pl()
+# ______________________
+
+# 2nd method: Another way to create insert statement is to use the standalone
+# insert() function from the sqlalchemy package:
+
+conn = engine.connect() 
+
+ins = insert(customers).values(
+    first_name = 'Katherine',
+    last_name = 'Wilson',
+    username = 'katwilson',
+    email = 'katwilson@gmail.com',
+    address = '4685 West Side Avenue',
+    town = 'Peterbrugh'
+)
+
+r = conn.execute(ins)
+print(r.inserted_primary_key)
+pl()
+# ______________________
+
+# 3rd method: Instead of passing values to the values() method as keyword
+# arguments, we can also pass them to the execute() method.
+
+
+r = conn.execute(insert(customers), first_name = "Tim",
+                                    last_name = "Snyder",
+                                    username = "timsnyder",
+                                    email = "timsnyder@mail.com",
+                                    address = "1611 Sundown Lane",
+                                    town = "Langdale")
+
+# ______________________
+print(r.inserted_primary_key)
+print(type(r.inserted_primary_key))
+pl()
+# ______________________
+
+# The execute() method is quite flexible because it allows us to insert multiple
+# rows by passing a list of dictionaries each representing a row to be inserted.
+
+r = conn.execute(insert(customers),[
+     {
+        "first_name": "John",
+        "last_name": "Lara", 
+        "username": "johnlara",
+        "email": "johnlara@mail.com",
+        "address": "3073 Derek Drive", 
+        "town": "Norfolk"
+    },
+    {
+         "first_name": "Sarah", 
+            "last_name": "Tomlin", 
+            "username": "sarahtomlin", 
+            "email":"sarahtomlin@mail.com",
+            "address": "3572 Poplar Avenue",
+            "town": "Norfolk"
+    },
+    {
+        "first_name": "Pablo", 
+            "last_name": "Gibson", 
+            "username": "pablogibson", 
+            "email":"pablogibson@mail.com",
+            "address": "3494 Murry Street",
+            "town": "Peterbrugh"
+    },
+    {
+        "first_name": "Pablo", 
+            "last_name": "Lewis", 
+            "username": "pablolewis", 
+            "email":"pablolewis@mail.com",
+            "address": "3282 Jerry Toth Drive",
+            "town": "Peterbrugh"
+    },
+])
+
+print(r.rowcount)
+pl()
+# ______________________________________________________________________
+
 # metadata.drop_all(engine)
 metadata.create_all(engine)
+
+
+
