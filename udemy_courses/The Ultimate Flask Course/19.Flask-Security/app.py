bf9fd2db996f9351c2c264e861005f865507662f
Chris Farrugia <foxcode9@gmail.com>
2020-02-12 10:33:43 +0100

    updates to 19.Flask-Security

--- udemy_courses/The Ultimate Flask Course/19.Flask-Security/app.py
+++ udemy_courses/The Ultimate Flask Course/19.Flask-Security/app.py
@@ -17,17 +17,28 @@ app.config['SECRET_KEY'] = uuid4().hex
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///p:/Projects/tutorials/Udemy/The Ultimate Flask Course/19.Flask-Security/security.db' 
 
 
-# This is used  to access the http://127.0.0.1:5000/register  page.
+# This is used  to access the registry page:      http://127.0.0.1:5000/register  
 app.config['SECURITY_REGISTERABLE'] = True
 
 # This is used to register: 
 app.config['SECURITY_PASSWORD_SALT'] = 'This_is_a_secret'
 
 # to remove the error:  AttributeError: 'NoneType' object has no attribute 'send'
+# since email is not setup yet
 app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
 
+# This is used  to allow the reset password page:  http://127.0.0.1:5000/reset
+app.config['SECURITY_RECOVERABLE'] = True
+
+# This is used  to allow the change password page:  http://127.0.0.1:5000/change
+app.config['SECURITY_CHANGEABLE'] = True
 
 
+# This is used to redirect to this route after chnage pass, change it to try:
+app.config['SECURITY_POST_CHANGE_VIEW'] = 'pass_changed'
+# to remove the error:  AttributeError: 'NoneType' object has no attribute 'send'
+# since email is not setup yet
+app.config['SECURITY_SEND_PASSWORD_CHANGE_EMAIL'] = False
 
 
 db = SQLAlchemy(app)
@@ -56,6 +67,13 @@ class User(db.Model, UserMixin):
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+# ______________________________________________________________________
+
+@app.route('/pass_changed')
+def pass_changed():
+    return '<h2>Password change successfully!</h2>'
+
+
 # ______________________________________________________________________
 
 # try the following url from :
