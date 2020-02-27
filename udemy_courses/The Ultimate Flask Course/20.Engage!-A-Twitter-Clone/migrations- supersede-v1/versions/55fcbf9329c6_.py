6b5fd69e14dc8989dad11a5843cf0c8dabf4abe6
Christopher Farrugia <foxcode9@gmail.com>
2020-02-27 15:57:37 +0100

    updated to chapter20

---
+++ udemy_courses/The Ultimate Flask Course/20.Engage!-A-Twitter-Clone/migrations- supersede-v1/versions/55fcbf9329c6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 55fcbf9329c6
+Revises: b898a0fea851
+Create Date: 2020-02-21 14:42:39.983446
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '55fcbf9329c6'
+down_revision = 'b898a0fea851'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
