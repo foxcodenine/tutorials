e73dcfba188d4839f0632da99bdd3a9fba5e390b
Chris Farrugia <foxcode9@gmail.com>
2020-03-03 12:55:12 +0100

    updates to chapter2

--- udemy_courses/The_Complete_JavaScript_Course_2019/chapter02_JavaScript_Language_Basics/script.js
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter02_JavaScript_Language_Basics/script.js
@@ -129,3 +129,65 @@ console.log(n);
 
 n /= 2;
 console.log(n);
+
+/*********************************
+ * If / else statments
+ */ 
+
+var firstName = 'John';
+var civilStatus = 'Single'; 
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!')
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+
+
+
+var firstName = 'Joe'
+var isMarried = true
+
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' is single!')
+}
+
+
+
+
+
+var massMark = 78; // kg 
+var heightMark = 1.69; // meters
+
+var massJohn = 92; // kg 
+var heightJohn = 1.95; // meters
+
+var MBIMark = massMark / (heightMark * heightMark)
+var MBIJohn = massJohn / (heightJohn * heightJohn)
+
+if (MBIMark > MBIJohn){
+    console.log('Mark\'s BMI is higher than John\'s BMI. ');
+} else {
+    console.log('John\'s BMI is higher than Mark\'s BMI. ');
+}
+
+/*********************************
+ * Boolean logic 
+ */ 
+
+
+var firstName = 'Dorothy'
+var age = 26
+
+if (age < 13){
+    console.log(firstName + ' is a girl!');
+} else if (age >= 13 && age < 20){
+    console.log(firstName + ' is a teenageer!');
+} else if (age >= 20 && age < 30){
+    console.log(firstName + ' is a young women!');
+} else {
+    console.log(firstName + ' is a woman!');
+}
