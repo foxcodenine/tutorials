db770c48cb2b59b1c6d8a17c4ea0f266a18daf93
Chris Farrugia <foxcode9@gmail.com>
2020-03-22 15:22:48 +0100

    revise flask POST and GET methods

--- tech_with_tim/flask_tutorials/tutorial3/app.py
+++ tech_with_tim/flask_tutorials/tutorial3/app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__) 
 
 app.config['DEBUG'] = True
+# ______________________________________________________________________
+# tutorial #3
 
 @app.route("/")
 def index():
-    return render_template('index.html', content='Testing')
+    return render_template('index.html', content='Testing Index Page')
 
 @app.route("/new_page")
 def new_page():
     return render_template('new_page.html')
+
+# ______________________________________________________________________
+# tutorial #4
+@app.route('/login', methods=['POST','GET'])
+def login():
+
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('user', usr=user))
+
+
+    return render_template('login.html')
+
+
+@app.route('/<usr>')
+def user(usr):
+    return f'<h2 >Yes they do {usr}!</h2>'
+
+
 
 if __name__ == '__main__':
     app.run()
