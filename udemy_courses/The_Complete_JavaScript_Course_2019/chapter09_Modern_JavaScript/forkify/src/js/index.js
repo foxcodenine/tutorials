5a96da494988e169b527e54f3d27f0c5856c994a
Chris Farrugia <foxcode9@gmail.com>
2020-05-16 14:16:54 +0200

    working_with_module & export/import

--- udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/index.js
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/index.js
@@ -1,4 +1,10 @@
-// Global app controller
-import num from './textModule';
-const x = 23;
-console.log(`I imported ${num} from another JavaScript module! Variable x is ${x}!`)
+import str from './models/Search' 
+import { myAddFunc as add, myMultipleFunc as multy} from './views/searchView'
+// import * as searchView from './views.searchView';
+
+
+console.log(str);
+
+console.log(add(2,2,2,20));
+
+console.log(multy(1,2,3,4,5,2));
---
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/models/Search.js
@@ -0,0 +1,1 @@
+export default `I'm an export string!`
