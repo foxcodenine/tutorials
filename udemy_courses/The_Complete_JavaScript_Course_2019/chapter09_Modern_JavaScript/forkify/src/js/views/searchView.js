b513f3ed309a4d396beff85d411b38c64f561b83
Christopher Farrugia <foxcode9@gmail.com>
2020-05-16 14:16:54 +0200

    working_with_module & export/import

---
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/views/searchView.js
@@ -0,0 +1,18 @@
+export 
+const myAddFunc = (...i) => {
+    let r = 0;
+    for( let n of i) {
+        r += n
+    };
+    return r;
+};
+
+export
+const myMultipleFunc = (...i) => {
+    let r = i.shift();
+    for(let n of i) {
+        r *= n
+    };
+    return r;
+};
+
