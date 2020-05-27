7e30f31f46c479d1e8426a0f0e122e60e8422209
Chris Farrugia <foxcode9@gmail.com>
2020-05-27 20:53:30 +0200

    update_recipe_model

--- udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/models/Recipe.js
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/models/Recipe.js
@@ -60,5 +60,87 @@ export default class Recipe {
         } catch (error) {
             console.log(error)
         }
+        
+    }
+
+    
+    parseIngredients() {
+
+        const unitsLong  = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds', 'tsps', 'tbsps']
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound', 'tsp', 'tbsp']
+
+        let newIngredients = this.ingredients.map(el => {
+
+            // 1) Grab values units and ingredened, to a string
+
+            let ingredient =  `${el.amount.us.value}|${el.amount.us.unit}|${el.name}`.toLocaleLowerCase();
+
+            // 2) Uniform units
+           
+            unitsLong.forEach((long, index) => {
+                ingredient = ingredient.replace(long, unitsShort[index]);
+            });
+
+            // 3) Remove partheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            // 4) Parse ingredients into count, unit and ingredient
+            ingredient = ingredient.split('|');
+            ingredient = {
+                count: Math.round(parseFloat(ingredient[0])*100)*0.01,
+                unit: ingredient[1], 
+                ingredient: ingredient[2]
+            };
+            return ingredient;
+
+        });
+
+        this.ingredients = newIngredients;
     }
 };
+
+
+
+/*
+
+//  Code not use case I have a different api; 
+arrIng = ['chris', 11111, 'tsp'];
+const unitIndex = arrIng.findIndex(el2 => unitsShort.includes('tsp'));
+
+
+let objIng;
+if (unitIndex > -1) {
+    // There is a unit
+    // Ex. 4 1/2 cups, arrCount is [4, 1/2] --> eval("4+1/2)")
+    // Ex. 4 cups, arrCount is [4]
+    const arrCount = arrIng.slice(0, unitIndex);
+
+    let count;
+    if (arrCount.length === 1) {
+        count = eval(arrIng[0].replace('-', '+'));
+    } else {
+        count = eval(arrIng.slice(0, unitIndex).join('+'));
+    }
+    objIng = {
+        count,
+        unit: arrIng[unitIndex], 
+        ingredient:arrIng.slice(unitIndex + 1).join(' ')
+    };
+
+
+} else if (parseInt(arrIng[0], 10)) {
+    // There is No unit, but 1st element is a number
+    objIng = {
+        count: parseInt(arrIng[0], 10),
+        unit: '',
+        ingredient: arrIng.slice(1).join(' ')
+    }
+} else if (unitIndex === -1) {
+    // There is No unit and No number in 1st position
+    objIng = {
+        count: 1,
+        unit: '',
+        ingredient: arrIng.join(' ')
+    }
+}
+*/
