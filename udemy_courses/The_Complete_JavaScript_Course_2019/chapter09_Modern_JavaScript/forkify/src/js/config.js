5b07d1cc7ec0c67b3c0c900029cdd35916801180
Chris <foxcode9@gmail.com>
2020-06-07 23:13:44 +0200

    building_likesView

--- udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/config.js
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/forkify/src/js/config.js
@@ -1,4 +1,4 @@
-export const myKey = 'b3bb9752f3d1449bbf6a1cf302da308e';
-
+// export const myKey = 'b3bb9752f3d1449bbf6a1cf302da308e';
+export const myKey = '57f70fde3bf84b6188194d0b5f8d4400';
 // 57f70fde3bf84b6188194d0b5f8d4400 
 // b3bb9752f3d1449bbf6a1cf302da308e
