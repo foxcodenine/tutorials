5082630ab62feea161f7c7e99ae2d88fd9a7d555
Christopher Farrugia <foxcode9@gmail.com>
2020-06-11 20:06:35 +0200

    setting up flask securite

--- udemy_courses/The_Ultimate_Flask_Course/22.Form-App/app.py
+++ udemy_courses/The_Ultimate_Flask_Course/22.Form-App/app.py
@@ -1,7 +1,15 @@
+# https://pythonise.com/series/learning-flask/flask-configuration-files
+# https://www.atlassian.com/git/tutorials/saving-changes/gitignore#git-ignore-patterns
+
+# pip install bcrypt
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
+
+
 
 app = Flask(__name__)
 
@@ -10,16 +18,46 @@ if app.config["ENV"] == "production":
 else:
     app.config.from_object("config.DevelopmentConfig")
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///p:/Projects/tutorials/Udemy/The_Ultimate_Flask_Course/22.Form-App/forum.db'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['DEBUG'] = True
-# app.config['SECRET_KEY'] = 'mysecret'
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
+# ______________________________________________________________________
+
+roles_user = db.Table('roles_users', 
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    descripton = db.Column(db.String(255))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_user, backref=db.backref('users', lazy='dynamic'))
+
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+Security = Security(app, user_datastore)
 # ______________________________________________________________________
 
 @app.route('/')
@@ -42,7 +80,17 @@ def login():
 def register():
     return render_template('register.html')
 
-print(app.config)
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+# ______________________________________________________________________
+import flask_security
+import os
+
+print('\n>>>>>>>> >>',os.path.dirname(flask_security.__file__))
+
+# ______________________________________________________________________
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
