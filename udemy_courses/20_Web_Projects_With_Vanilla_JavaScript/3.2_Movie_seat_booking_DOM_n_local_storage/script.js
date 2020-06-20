4a0437e396211d698974f25cabbf3d01933a319a
Christopher Farrugia <foxcode9@gmail.com>
2020-06-20 15:36:41 +0200

    added_css_to_project

---
+++ udemy_courses/20_Web_Projects_With_Vanilla_JavaScript/3.2_Movie_seat_booking_DOM_n_local_storage/script.js
@@ -0,0 +1,7 @@
+const elements = {
+    container: document.querySelector('.container'),
+    seats: document.querySelectorAll('.row .seat:not(.occupied)'),
+    count: document.getElementById('count'),
+    total: document.getElementById('total'),
+    movieSelect: document.getElementById('movie')
+}
