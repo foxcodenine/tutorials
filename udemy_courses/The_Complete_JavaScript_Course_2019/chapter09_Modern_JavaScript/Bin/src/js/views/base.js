b77ad3e19c09a06eda5c6ee0ccb7ffbdf4f8ce6e
Chris <foxcode9@gmail.com>
2020-06-25 20:46:14 +0200

    bugs_fixing

---
+++ udemy_courses/The_Complete_JavaScript_Course_2019/chapter09_Modern_JavaScript/Bin/src/js/views/base.js
@@ -0,0 +1,38 @@
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results'),
+    searchResPages: document.querySelector('.results__pages'),
+    recipeContainer: document.querySelector('.recipe'),
+    
+};
+// _____________________________________________________________________
+
+export const elementStrings = {
+    loader: 'loader'  // <- so we can use it in both cases (A)
+}
+
+
+
+// _____________________________________________________________________
+
+export const renderLoader = parent => {
+    const loader = `
+    <div class="${elementStrings.loader}"> 
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use> 
+        </svg>
+    </div> 
+    `; // <- (A)
+    parent.insertAdjacentHTML('afterbegin', loader);    
+}; 
+
+// _____________________________________________________________________
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`); // <- (A)
+    if (loader) {
+        loader.parentElement.removeChild(loader)
+    };
+}
