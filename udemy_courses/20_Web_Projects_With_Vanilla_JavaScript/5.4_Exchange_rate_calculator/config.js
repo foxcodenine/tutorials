ad099d6ae3b40e56be3702b687cb170c475150d9
Chris Farrugia <foxcode9@gmail.com>
2020-06-30 20:39:29 +0200

    add javascript to project

---
+++ udemy_courses/20_Web_Projects_With_Vanilla_JavaScript/5.4_Exchange_rate_calculator/config.js
@@ -0,0 +1,7 @@
+export const myKey = '821182602159aedb4f7a2042';
+
+
+// https://www.exchangerate-api.com/app/dashboard
+// https://www.exchangerate-api.com/docs/standard-requests
+
+// GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD
