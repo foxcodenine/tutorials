32855cbf7e369c42f2fedc7fbbc425050731d859
Chris <foxcode9@gmail.com>
2020-06-30 20:37:24 +0200

    flask_wtf - html5[URLField, DateField] SubmitField] validators[URL, EqualTo] and sass recaptcha]

--- hackers_and_slackers/part3_wtforms/app.py
+++ hackers_and_slackers/part3_wtforms/app.py
@@ -5,11 +5,12 @@
 
 
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, email
+from flask_wtf.html5 import URLField, DateField
+from wtforms import StringField, TextField, SubmitField, PasswordField, SelectField
+from wtforms.validators import DataRequired, Length, Email, URL, EqualTo
 # ______________________________________________________________________
 
 app = Flask(__name__)
@@ -19,9 +20,6 @@ if app.config['ENV'] == 'development':
 else:
     app.config.from_object('config.ConfigProduction')
 
-
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lc6nsAUAAAAAOFGdJSc0w9UEpcqiDY2wt8kjse6'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lc6nsAUAAAAAO_Sg79VFqPZO1iS05NT-Q4RZnKw'
 # ______________________________________________________________________
 
 class ContactForm(FlaskForm):
@@ -30,10 +28,8 @@ class ContactForm(FlaskForm):
 
     email = StringField(
         'Email', validators=[
-            DataRequired(message='Email field is required.'), Email(message='Not a valid email address.'),
-            
-            ])
-    
+            DataRequired(message='Email field is required.'), Email(message='Not a valid email address.'),            
+            ])    
     message = TextField('Message', validators=[
         DataRequired(message='Message field is required.'), Length(min=4, message='Your message is too short.')
             ])
@@ -42,6 +38,24 @@ class ContactForm(FlaskForm):
     recaptcha = RecaptchaField()
 
 class SignupForm(FlaskForm):
+    email = TextField('Email', validators=[
+        DataRequired(message='Email field is required.'), 
+        Email(message='Not a valid email address.')
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(message='Password field is required.'), 
+        Length(min=6, max=8, message='Password must be between 6 to 15 characters long.')
+    ])
+
+    repeat_password = PasswordField('Repeat Password ', validators=[
+        DataRequired(message='Password field is required.'), EqualTo('password', message='Passwords does not match.')])
+
+    gender = SelectField('Gender', choices=[('m', 'Male'),('f', 'Female'),('o', 'Others')])
+
+    website = URLField('Website', validators=[DataRequired(message='Website is required.'), URL()])
+
+    birthday = DateField('Your Birthday', validators=[DataRequired(message=' Your birthday is required.')])
+        
     recaptcha = RecaptchaField()
 
 # ______________________________________________________________________
@@ -60,9 +74,58 @@ def contact():
     form = ContactForm() 
 
     if form.validate_on_submit():
-        return render_template('blank.html', content='form submitted successfully')
+
+        _name = form.name.data
+        _email = form.email.data
+        _message = form.message.data
+
+        body = [
+            {'Name': _name},
+            {'Email': _email},
+            {'Message': _message}
+        ]
+
+        return render_template('blank.html', content='form submitted successfully', body=body)
 
     return render_template('contact.html', form=form)
+
+# _____________________________
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+
+    form = SignupForm()
+
+    if form.validate_on_submit():
+
+        email = form.email.data
+        password = form.password.data
+        repeat_password = form.repeat_password.data
+        gender = form.gender.data
+        website = form.website.data
+        birthday = form.birthday.data
+
+        # if password != repeat_password:
+        #     flash('Password does not match.')
+        #     return redirect(url_for('signup'))
+
+        body = [
+            {'email': email},
+            {'password': password},
+            {'repeat_password': repeat_password},
+            {'gender': gender},
+            {'website': website},
+            {'birthday': birthday}
+        ]
+
+
+        return render_template('blank.html', content='Singup form submitted successfully', body=body)
+
+    return render_template('sign_up.html', form=form)
+
+
+print('>>>>', app.config['CHECK_ENV'])
+
 
 # ______________________________________________________________________
 if __name__ == "__main__":
