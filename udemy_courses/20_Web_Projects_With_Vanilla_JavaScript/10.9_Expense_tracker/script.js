33d81a5c395c245aea0bf971e363b890de6f2d16
Chris <foxcode9@gmail.com>
2020-07-13 20:54:41 +0200

    started_project_10.9

---
+++ udemy_courses/20_Web_Projects_With_Vanilla_JavaScript/10.9_Expense_tracker/script.js
@@ -0,0 +1,35 @@
+
+
+// _____________________________________________________________________
+
+// DOM elements
+
+const elem = {
+    balance: document.getElementById('balance'),
+    money_minus: document.getElementById('money-minus'),
+    money_plus: document.getElementById('money-plus'),
+    list: document.getElementById('list'),
+    form: document.getElementById('form'),
+    text: document.getElementById('text'),
+    amount: document.getElementById('amount'),
+}
+
+const dummyTransection = [
+    {id: 1, text: 'Flowers', amount: -20},
+    {id: 2, text: 'Salary', amount: 300},
+    {id: 3, text: 'Book', amount: -10},
+    {id: 4, text: 'Camera', amount: 150},
+]
+
+let transection = dummyTransection;
+// _____________________________________________________________________
+
+// Add transaction to DOM list
+
+
+// _____________________________________________________________________
+
+
+
+
+// _____________________________________________________________________
