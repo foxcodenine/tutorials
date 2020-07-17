a5b436da7ed11104e13e3dc7add14808fdd6f78f
Christopher Farrugia <foxcode9@gmail.com>
2020-07-17 23:27:09 +0200

    js_waypoint

--- udemy_courses/Advanced_CSS_and_Sass_Flexbox_Grid_Animations_and_More!/chapter_4_Sass_&_NPM/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('click', e=>{
-    const mainBtn = e.target.matches('.btn--animated, .btn--animated *')
-
-    if (mainBtn) setTimeout(()=>{
-        window.location.reload();
-    }, 100);
-});
