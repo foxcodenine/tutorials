5f82ac01d00b8978d7f78ea3fada98c8831220c1
Christopher Farrugia <foxcode9@gmail.com>
2020-07-22 20:54:27 +0200

    started_project_11

---
+++ udemy_courses/20_Web_Projects_With_Vanilla_JavaScript/11.10_Music_player_html5_audio_api/script.js
@@ -0,0 +1,17 @@
+const elem = {
+    musicContainer: document.getElementById('music-container'),
+    
+    playBtn: document.getElementById('play'),
+    prevBtn: document.getElementById('prev'),
+    nextBtn: document.getElementById('next'),
+
+    audio: document.getElementById('audio'),
+    progress: document.getElementById('progress'),
+    progressContainer: document.getElementById('progress-container'),
+
+    title: document.getElementById('title'),
+    cover: document.getElementById('cover'),
+}
+
+// Song title
+const songs = ['hey', 'summer', 'ukulele'];
