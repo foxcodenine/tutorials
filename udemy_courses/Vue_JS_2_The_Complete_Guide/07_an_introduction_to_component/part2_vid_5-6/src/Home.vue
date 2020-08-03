a91f1b8af975e7b69b123e926aeebdd0116f3141
Chris Farrugia <foxcode9@gmail.com>
2020-08-03 21:02:36 +0200

    Vue_chapter7

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/07_an_introduction_to_component/part2_vid_5-6/src/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>Hello {{product}} World!</h1>
+    <br>
+    <button v-on:click="changeProduct()">Change</button>  
+  </div>
+</template>
+
+<script>
+  export default { 
+      data: function() {
+        return {
+          product: 'Vue'
+        }
+      },
+      methods: {
+        changeProduct() {
+          this.product = 'Python';
+        }
+      }
+    }
+</script>
+
+<style>
+  @import "../style.css";
+</style>
