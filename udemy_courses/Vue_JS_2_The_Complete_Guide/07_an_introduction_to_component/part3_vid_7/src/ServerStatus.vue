01ab04dac44c464e47e844b9d31e453f965a8237
Christopher Farrugia <foxcode9@gmail.com>
2020-08-03 21:02:36 +0200

    Vue_chapter7

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/07_an_introduction_to_component/part3_vid_7/src/ServerStatus.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <p> 
+            <button v-on:click="changeStatus()">Change Status</button>
+            &nbsp;&nbsp;&nbsp;Server Status: <span v-html="status"></span>
+        </p>
+        
+        
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: '<b>Critical</b>'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
+
+<style>
+  @import "../style.css";
+</style>
