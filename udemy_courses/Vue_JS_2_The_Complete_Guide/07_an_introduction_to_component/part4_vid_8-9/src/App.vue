c4c7774e50213deea06a2630720f4a112bdb1b36
Chris Farrugia <foxcode9@gmail.com>
2020-08-04 19:02:43 +0200

    vue_practice_project

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/07_an_introduction_to_component/part4_vid_8-9/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <app-servers></app-servers>
+    <app-details></app-details>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Header from './Header';  
+  import Servers from './Servers';
+  import Details from './ServerDeatils';
+  import Footer from './Footer';
+
+  export default {
+    components: {
+      'app-header': Header,      
+      'app-servers': Servers,
+      'app-details': Details,
+      'app-footer': Footer
+    }
+  }
+
+</script>
+
+<style lang="scss">
+
+</style>
