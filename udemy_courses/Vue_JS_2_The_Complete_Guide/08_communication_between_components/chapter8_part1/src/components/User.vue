197820ef53af4182dfb32b7931fb92da105575bd
Christopher Farrugia <foxcode9@gmail.com>
2020-08-06 21:05:16 +0200

    vue_chapter_8_components

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/08_communication_between_components/chapter8_part1/src/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1> 
+        <p>I am an awesome User!</p>
+        <button v-on:click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">            
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail v-bind:my_name="name" v-on:nameWasReset='name = $event'></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>        
+    </div>
+</template>
+
+<script>
+    import UserEdit from './UserEdit'
+    import UserDetail from './UserDetail'
+
+export default {
+    components: {
+        appUserEdit: UserEdit,
+        appUserDetail: UserDetail,
+    },
+    data: function() {
+        return {
+            name: 'Christopher'
+        };
+    },
+    methods: {
+        changeName() {
+            this.name = 'James'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
