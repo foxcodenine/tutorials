659a076c31a7e5fc6453a837cad51aae62bb3dc7
Chris Farrugia <foxcode9@gmail.com>
2020-08-06 21:05:16 +0200

    vue_chapter_8_components

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/08_communication_between_components/chapter8_part1/src/components/UserDetail.vue
@@ -0,0 +1,50 @@
+<template>
+        <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button v-on:click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // props: ["my_name"],
+
+        // props: {
+        //     my_name: [String]
+        // },
+
+        props: {
+            my_name: {
+                type: String,
+                default: 'Max',
+                // required: true
+            }
+        },
+        // props: {
+        //     my_name: {
+        //         type: Object,
+        //         default: function() {
+        //             return {name: 'Christopher'}
+        //         },                
+        //     }
+        // },
+        methods: {
+            switchName() {
+                return  this.my_name.split('').reverse().join("");
+            },
+            resetName() {
+                this.my_name = 'Christopher';
+                this.$emit('nameWasReset', this.my_name)
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
