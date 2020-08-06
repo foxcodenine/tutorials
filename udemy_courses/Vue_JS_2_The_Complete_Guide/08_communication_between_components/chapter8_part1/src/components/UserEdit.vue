a86477f0a5532fe1a6f186da51038ba0352248f1
Chris <foxcode9@gmail.com>
2020-08-06 21:05:16 +0200

    vue_chapter_8_components

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/08_communication_between_components/chapter8_part1/src/components/UserEdit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="component">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: lightgreen;
+    }
+</style>
