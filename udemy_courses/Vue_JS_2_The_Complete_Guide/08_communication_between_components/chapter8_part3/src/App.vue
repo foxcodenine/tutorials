b1f300f7190a2f6577c922988725b5f2aaac1dda
Chris <foxcode9@gmail.com>
2020-08-07 22:54:08 +0200

    completed_chapter8

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/08_communication_between_components/chapter8_part3/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <app-User></app-User>
+        </div>
+      </div>
+    </div>
+
+  
+</template>
+
+<script>
+  import User from './components/User'
+
+  export default {
+    components: {
+      appUser: User
+    }
+  }
+</script>
+
+<style lang="scss">
+  div.component {
+    border: 1px solid black;
+    padding: 30px;
+  }
+</style>
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/08_communication_between_components/chapter8_part3/src/components/User.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1> 
+        <p>I am an awesome User!</p>
+        <button v-on:click="changeName">Change my Name</button>        
+        <p>Name: {{ name }}</p>
+        <p>Age: {{ age }}</p>
+        <hr>
+        <div class="row">            
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail v-bind:my_name="name" 
+                                 v-on:nameWasReset='name = $event'
+                                 v-bind:resetFunction="restName"
+                                 v-bind:userAge='age'
+                                 ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                        v-bind:userAge='age'
+                        v-on:ageWasEdit='age=$event'></app-user-edit>
+            </div>
+        </div>        
+    </div>
+</template>
+
+<script>
+    import UserEdit from './UserEdit'
+    import UserDetail from './UserDetail'
+
+export default {
+    components: {
+        appUserEdit: UserEdit,
+        appUserDetail: UserDetail,
+    },
+    data: function() {
+        return {
+            name: 'Christopher',
+            age: 27,
+        };
+    },
+    methods: {
+        changeName() {
+            this.name = 'James'
+        },
+        restName() {
+            this.name = 'Christopher'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
