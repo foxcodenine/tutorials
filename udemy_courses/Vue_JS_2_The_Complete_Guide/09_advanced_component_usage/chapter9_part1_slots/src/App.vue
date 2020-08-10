ec9c4a02d2f581268f7ffae0fe23cf5dc2a5dc24
Christopher Farrugia <foxcode9@gmail.com>
2020-08-10 20:31:26 +0200

    vue_chapter9_start_finishaed

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/09_advanced_component_usage/chapter9_part1_slots/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="conatainer">
+    <div class="row">
+      <div class="col-xs-12">
+
+        <app-quote quote="A Wonderfull Quote!"> 
+          <h2>Data passed to slot</h2>
+          <p>{{message}}</p>
+          <p slot='content'>{{message}} - (named slot)</p>
+          <!-- <p slot='subtitle'>Test Subtitle</p> -->
+        </app-quote>
+        
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import  Quote  from './components/Quote'
+  export default {
+    
+    data: ()=>{
+      return {
+        message: 'Data passed from AppData to slot'
+      }
+    },
+
+    components: {
+      'app-quote': Quote,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  p {
+    color: darkblue;
+  }
+  h2 {
+    color: chartreuse;
+  }
+</style>
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/09_advanced_component_usage/chapter9_part2_dynamic_components/src/components/Blue.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>This is the Blue component</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+     div {
+        border: 1px solid blue;
+        
+        padding: 10px 20px;
+        background: rgba(blue, .2);
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
