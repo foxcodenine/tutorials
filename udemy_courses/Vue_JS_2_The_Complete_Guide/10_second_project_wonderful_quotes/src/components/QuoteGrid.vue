4275e96c43d83b21baa1f9f742846f2a1c62fcee
Christopher Farrugia <foxcode9@gmail.com>
2020-08-12 21:51:45 +0200

    complete_vue_chapter10

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/10_second_project_wonderful_quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <app-quote 
+            v-for='(quote, index) in quotes' 
+            v-bind:key='index' 
+            v-on:click.native='deleteQuote(index)'>{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+
+    import Qoute from './Qoute'
+
+    export default {
+        props: ['quotes'],
+
+        components: {
+            appQuote: Qoute
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleteIndex', index);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
