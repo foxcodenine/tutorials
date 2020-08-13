a10ee6a869f0940e33804e48cd6e6abad9c7ad35
Chris Farrugia <foxcode9@gmail.com>
2020-08-13 20:41:48 +0200

    start_and_finished_chapter_11

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/11_vue_forms/forms/src/Switch.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+
+        <div
+            id="on"
+            v-on:click="switched(true)"
+            v-bind:class="{active: value}"
+        >On</div>
+
+        <div
+            id="off"
+            v-on:click="switched(false)"
+            v-bind:class="{active: !value}"
+        >Off</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+
+        methods: {
+            switched(i) {                
+                this.$emit('input', i);
+
+                // v-model will bind value from app &
+                //              listen for the input event
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    #on, #off {
+        width: 4rem;
+        height: 2rem;
+        background-color: lightgray;
+        padding: .2rem;
+        display: inline-block;
+        margin: 1rem -.2rem;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+    
+</style>
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/11_vue_forms/practice/src/FullName.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="form-group">
+          <label>Name: </label>
+          <input type="text" class="form-control" v-model.lazy="name">
+        </div>
+        
+        <div class="form-group">
+          <label>Surname: </label>
+          <input type="text" class="form-control" v-model.lazy="surname">
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+
+        data() {
+            return {
+                name: '',
+                surname: ''
+            }
+        },
+        
+        watch: {
+            name() {
+                this.value= `${this.name} ${this.surname}`  
+            },
+            surname() {
+                this.value = `${this.name} ${this.surname}`  
+            },
+            value() {
+                this.$emit('input', this.value)
+            }
+        },
+    }
+</script>
