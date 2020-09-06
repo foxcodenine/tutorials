5b03c1a53132ce361ffd2ff5ef461e430cb1d634
Chris <foxcode9@gmail.com>
2020-09-06 09:47:15 +0200

    update_the_monster_slayer_app_with_components_and_animations

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/14_monster_slayer/src/components/DamageReport.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="row log" >
+
+        <div class="small-12 columns">
+            <ul>
+                <transition-group name="fade" appear>
+                    <li v-for="turn in turns.slice().reverse()"
+                        v-bind:class="{'player-turn': turn.isPlayer, 'monster-turn': !turn.isPlayer}" :key="turn.index">
+                        {{ turn.text }}
+                    </li>
+                </transition-group>
+            </ul>
+        </div>
+
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+        props: ['turns'],
+    }
+
+</script>
+
+<style>
+    .fade-enter {
+    opacity: 0;
+  }
+    .fade-enter-active {
+
+    transition: all 1.4s;
+  }
+    .fade-leave {
+    opacity: 1;
+  }
+  .fade-leave-active {
+    opacity: 0;
+    transition: all .7s;
+  }
+  .fade-move {
+    transition: all .7s;
+  }
+</style>
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/14_monster_slayer/src/components/StartNewGame.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="row controls" >
+
+        <div class="small-12 columns">
+            <button id="start-game" v-on:click='newGame'>START NEW GAME</button>
+        </div>
+
+    </section>
+</template>
+
+<script>
+
+    export default {
+        props: ['gameIsRunning', 'playerHealth', 'monsterHealth'],
+
+        methods: {
+            newGame() {
+                this.$emit('newGame')
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
