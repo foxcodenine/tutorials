f0ebc85ced943159607dcf290552918c383fe175
Chris Farrugia <foxcode9@gmail.com>
2020-09-06 09:47:15 +0200

    update_the_monster_slayer_app_with_components_and_animations

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/14_monster_slayer/src/components/Buttons.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <section class="row controls" >
+        <div class="small-12 columns">
+            <button id="attack" v-on:click='btnAttack'>ATTACK</button>
+            <button id="special-attack" v-on:click='btnSpecialAttack' v-if='showSpecialAttack'>SPECIAL ATTACK</button>
+            <button id="heal" v-on:click='btnHeal'>HEAL</button>
+            <button id="give-up" v-on:click='btnGiveUp'>GIVE UP</button>
+        </div>
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+        props:['gameIsRunning', 'playerHealth', 'monsterHealth'],
+
+        data() {
+            return {
+                showSpecialAttack: false
+            }
+        },
+
+        methods: {
+            btnAttack() {
+                this.$emit('btnAttack');
+                this.specialAttackOn();
+            },
+            btnSpecialAttack() {
+                this.$emit('btnSpecialAttack');
+                this.specialAttackOn();
+            },
+            btnHeal() {
+                this.$emit('btnHeal');
+                this.specialAttackOn();
+            },
+            btnGiveUp() {
+                this.$emit('btnGiveUp');
+            },
+            specialAttackOn() {
+
+                if (!(this.playerHealth % 6 && this.monsterHealth %6)) {
+                    this.showSpecialAttack = true;
+                } else {
+                    this.showSpecialAttack = false;
+                }
+            },
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
