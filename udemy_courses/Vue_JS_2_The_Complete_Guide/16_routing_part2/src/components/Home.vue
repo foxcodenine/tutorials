b40c84d8c4fab3754369b8847e37f507f1addafb
Christopher Farrugia <foxcode9@gmail.com>
2020-09-10 21:02:32 +0200

    update_to_chapter16_vue_routing

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/16_routing_part2/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>The Home Page</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Provident unde quasi quae iusto molestias ipsam fugit fuga earum
+        excepturi pariatur, veniam nostrum consectetur placeat, ut nisi!
+        Iure et quia eaque.</p>
+    </div>
+</template>
