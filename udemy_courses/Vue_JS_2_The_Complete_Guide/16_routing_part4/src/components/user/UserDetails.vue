d86dc2611c86de51d624c642c464b0f0083c7e6c
Chris Farrugia <foxcode9@gmail.com>
2020-09-11 21:34:25 +0200

    updates

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/16_routing_part4/src/components/user/UserDetails.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h3>Some User Details</h3>
+        <h4>User loaded ID: {{ $route.params.id }}</h4>
+        <!-- <router-link 
+                tag="button"                 
+                :to="'$route.parmas.id' + '/edit'"
+                class="btn btn-primary" >Edit</router-link> -->
+
+        <router-link 
+            tag="button"                 
+            :to="link"
+            class="btn btn-primary" >Edit</router-link>
+                
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+                link: {
+                    name: 'userEditPath',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'mt', 
+                        q: 100
+                    },
+                    hash: '#copyright'
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            console.log('>> detail beforeRouteEnter to.path:', to.path)      
+            
+            if (to.path.split('/')[2] <= 2) {
+                next();
+            } else {
+                console.log('>> redirected to homepage')
+                next({path: '/'});
+            }            
+        },
+    }
+</script>
+
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/16_routing_part4/src/components/user/UserStart.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Please select a User</p>
+        <hr>
+        <ul class="list-group">
+            <router-link tag="li" class="list-group-item" to="user/1">User 1</router-link>
+            <router-link tag="li" class="list-group-item" to="user/2">User 2</router-link>
+            <router-link tag="li" class="list-group-item" to="user/3">User 3</router-link>
+        </ul>
+    </div>
+</template>
+
+
+
+<style scoped>
+    .list-group-item {
+        cursor: pointer;
+    }
+    .list-group-item:hover {
+        border: 1px solid red;
+        z-index: 999;
+    }
+</style>
