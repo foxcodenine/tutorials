2ab7cc3580ffe9e59074c79c901be822a2106762
Chris <foxcode9@gmail.com>
2020-09-16 18:45:07 +0200

    finished_chapter17_vue_vuex

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/17_vue_vuex_advanced/src/store/actions.js
@@ -0,0 +1,9 @@
+export const updateRedValue = ({commit}, payload) => {
+    commit('updateRedValue', payload);
+}
+
+export const updateBlueValue = ({commit}, payload) => {
+    commit('updateBlueValue', payload);
+}
+
+
