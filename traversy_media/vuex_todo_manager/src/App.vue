41999645c7397c09648456a2146621f4c3e6b65d
Christopher Farrugia <foxcode9@gmail.com>
2020-09-17 20:30:55 +0200

    started vuex todo app

---
+++ traversy_media/vuex_todo_manager/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" >
+    <div class="container">
+ 
+      <app-add-todos></app-add-todos>
+      <app-todos></app-todos>
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  import Todos from "./components/Todos";
+  import AddTodos from './components/AddTodos'
+  export default {
+    name: 'app',
+
+    components: {
+      'appTodos': Todos,
+      'appAddTodos': AddTodos,
+    }
+  }
+</script>
+
+<style lang="scss">
+  body {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    line-height: 1.6;
+    background:  #e8f7f0;
+  }
+  .container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+  }
+</style>
---
+++ traversy_media/vuex_todo_manager/src/components/AddTodos.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <h3>Add Todos</h3>
+      <div class="add">
+              <form>
+        <input type="text" v-model="title" placeholder="Add todo...">
+        <button>Submit</button>
+        <!-- <input type="submit"> -->
+      </form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+    input[type='text']{
+      flex: 10;
+      padding: 10px;
+      border: 1px solid #41b883;
+      outline: 0;
+    }
+    input[type='submit'],
+    button {
+      flex: 2;
+      background:  #41b883;
+      color: #fff;
+      cursor: pointer;
+      border: 1px solid #41b883;
+    }
+</style>
