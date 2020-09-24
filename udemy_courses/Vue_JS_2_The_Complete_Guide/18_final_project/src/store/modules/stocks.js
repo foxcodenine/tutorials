eef73d90a6937d18759ffaad5a3d52f9c7e46905
Chris Farrugia <foxcode9@gmail.com>
2020-09-24 20:25:47 +0200

    vue_chapter18_final_project

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/18_final_project/src/store/modules/stocks.js
@@ -0,0 +1,42 @@
+import stocks from '../../data/stocks'
+
+const state = {
+    stocks: []
+};
+
+const getters = {
+    stocks(state) {
+        return state.stocks
+    }
+};
+
+const mutations = {
+    'SET_STOCKS'(state, stocks) {
+        state.stocks = stocks;
+    },
+    'RND_STOCKS'(state) {
+
+    }
+};
+
+const actions = {
+    buyStock(context, order) {
+        context.commit();
+    },
+    initStocks(context) {
+        context.commit('SET_STOCKS', stocks);
+    },
+    randomizeStocks(context) {
+        context.commit('RND_STOCKS');
+    }
+};
+
+
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations, 
+    actions
+};
