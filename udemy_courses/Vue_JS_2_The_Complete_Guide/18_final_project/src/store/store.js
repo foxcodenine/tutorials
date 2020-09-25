befa0ec029d08147dfc467d6c4e1ba0016122633
Chris <foxcode9@gmail.com>
2020-09-25 22:48:23 +0200

    vue_chapter18_updates

--- udemy_courses/Vue_JS_2_The_Complete_Guide/18_final_project/src/store/store.js
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/18_final_project/src/store/store.js
@@ -3,11 +3,13 @@ import Vuex from "vuex";
 
 
 import stocks from "./modules/stocks";
+import portfolio from "./modules/portfolio";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        stocks
+        stocks,
+        portfolio
     }
 });
