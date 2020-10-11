db2a8f2901f5ab4ab2a34bcc1c5d04f1266eb78a
Chris Farrugia <foxcode9@gmail.com>
2020-10-11 18:56:41 +0200

    chapter25_vue-cli3_completed_course

---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/25_vue-cli3/npm.md
@@ -0,0 +1,18 @@
+To install vue-cli 3 or 4 uninstall 2
+    $ sudo npm uninstall vue-cli -g
+
+
+And to install the new cli:
+    $ sudo npm install  @vue/cli -g
+
+And to use still use vue init:
+    $ sudo npm install -g @vue/cli-init
+
+To install @vue/cli-service-global
+    $ sudo npm install -g @vue/cli-service-global
+
+    and to run it:
+    $ vue serve componentName.vue
+
+To add/install vuetify to project:
+    $ vue add vuetify
---
+++ udemy_courses/Vue_JS_2_The_Complete_Guide/25_vue-cli3/standAloneComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h1>Hello there!</h1>
+        <p>This is a stand alone conponent used for testing.</p>
+        <small>To inspact a stand alone component on its own all you need to do
+        is:</small>
+        <ul>
+            <li>$ sudo npm i -g @vue/cli-service-global</li>
+            <li>$ vue serve componentName.vue</li>
+        </ul>
+  </div>
+</template>
+
+<style scoped>
+    * {
+        margin: 10px 0;
+        padding: 0;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 70vh;
+        font-family: 'Courier New', Courier, monospace;
+        width: 100%;
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+    li {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 12px;
+        margin: 7px 0;
+        color: darkblue;
+    }
+</style>
