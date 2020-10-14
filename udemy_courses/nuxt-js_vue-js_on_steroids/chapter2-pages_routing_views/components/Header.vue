1bf8a06e2380c7c96ffcdb5b501fea34b2414f3a
Christopher Farrugia <foxcode9@gmail.com>
2020-10-14 10:40:55 +0200

    nuxt_complete_chapter2

---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter2-pages_routing_views/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+      <ul>
+          <nuxt-link tag="li" to="/">Home</nuxt-link>
+          <nuxt-link tag="li" to="/users">User</nuxt-link>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        width: 100%;
+        padding: 10px 0;
+        background: #00C58E;
+
+        ul {
+            display: flex;
+            justify-content: flex-end;
+            
+        }
+
+        li {
+            display: inline-block;
+            margin: 0 10px;
+            background: transparent;
+            color: #3b8070;
+            padding: 10px 30px;
+            border-radius: 4px;
+            border: 1px solid #3b8070;
+            text-decoration: none;
+
+                &:hover {
+                    color: #fff;
+                    background-color: #3b8070;
+                }
+            }
+        }
+
+</style>
