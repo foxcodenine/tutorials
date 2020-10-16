df82233c4bdd5551d83d4b996aee93fcc5dbbf1f
Chris Farrugia <foxcode9@gmail.com>
2020-10-16 10:01:01 +0200

    nuxt chapter3_project

---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter3-project/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang='scss'>
+  html {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+
+  // *,
+  // *::before,
+  // *::after {
+  //   box-sizing: border-box;
+  //   margin: 0;
+  // }
+</style>
