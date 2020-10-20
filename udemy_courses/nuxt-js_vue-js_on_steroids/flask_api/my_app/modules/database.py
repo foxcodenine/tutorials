4bf0f7a0a0c7d51b126ca9c5ecbac3f440f4409d
Christopher Farrugia <foxcode9@gmail.com>
2020-10-20 13:12:37 +0200

    nuxt-chapter3 updates

---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/flask_api/my_app/modules/database.py
@@ -0,0 +1,21 @@
+from my_app import db
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    sample_text = db.Column(db.String(255), nullable=False)
+    thumbnail = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, title, author, sample_text, thumbnail):
+        self.title = title
+        self.author = author 
+        self.sample_text = sample_text 
+        self.thumbnail = thumbnail
+    
+    def __repr__(self):
+        return f'<Post id: {self.id}>'
+
