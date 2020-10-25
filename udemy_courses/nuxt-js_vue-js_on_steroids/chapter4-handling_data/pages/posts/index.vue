d64711eaa224eab8b7f73361528fe7ebc92548a1
Chris <foxcode9@gmail.com>
2020-10-25 19:16:19 +0100

    completed chapter4 and updated flask backend

--- udemy_courses/nuxt-js_vue-js_on_steroids/chapter4-handling_data/pages/posts/index.vue
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter4-handling_data/pages/posts/index.vue
@@ -12,26 +12,43 @@
         components: {
             PostSection
         },
-    //     data() {
-    //         return {
-    //             dataPost: []
-    //         }
-    //     },
-        asyncData() {
-            return fetch(
-                'http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}}
-                )
-                .then(res => res.json())
-                .then(data => {
-                    return {dataPost : [...data].reverse()}
-                }
-            )
-        },
-    //     created() {
-    //         fetch('http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}})
-    //         .then(res => res.json())
-    //         .then(data => this.dataPost = [...data].reverse())
-    //     }
+    // _________________________________________________________________
+        // Superseded code - Using asyncData instaed
+
+        // data() {
+        //     return {
+        //         dataPost: []
+        //     }
+        // },
+        // created() {
+        //     fetch('http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}})
+        //     .then(res => res.json())
+        //     .then(data => this.dataPost = [...data].reverse())
+        // },
+    // _________________________________________________________________
+
+        // Superseded code - Using nuxtServerInit(vuexContext, context) in store instead
+        // And fetch data from store with getter in computed
+    
+        // asyncData() {
+        //     return fetch(
+        //         'http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}}
+        //         )
+        //         .then(res => res.json())
+        //         .then(data => {
+        //             return {dataPost : [...data].reverse()}
+        //         }
+        //     )
+        // },
+        
+    // _________________________________________________________________
+        computed: {
+            dataPost() {
+                const loadedPost = this.$store.getters.fetchPost;
+                return loadedPost;
+            }
+        }
+
     }
 </script>
 
