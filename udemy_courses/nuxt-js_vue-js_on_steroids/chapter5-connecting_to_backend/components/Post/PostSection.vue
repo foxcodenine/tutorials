dcb9ef0949aec26248f1104fca24059eb5564018
Christopher Farrugia <foxcode9@gmail.com>
2020-10-26 07:30:32 +0100

    started chapter5 - add firebase backend

---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter5-connecting_to_backend/components/Post/PostSection.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="featured-post post" >
+      <PostPreview
+          v-for="p in dataPost" :key="p.id"
+          :id="p.id"
+          :title="p.title"
+          :is-admin="isAdmin"
+          :sampleText="p.sampleText"
+          :thumbnail="p.thumbnail">
+      </PostPreview>
+      
+    </section>
+</template>
+
+<script>
+    // import postData from "@/Data/postList";
+    import PostPreview from "@/components/Post/PostPreview";
+
+    export default {
+        props: {
+          isAdmin: {
+            type: Boolean,
+            default: false
+          },
+          dataPost: {
+            type: Array,
+            required: true
+          }
+        },
+        components: {
+            PostPreview
+        },
+        // data() {
+        //     return {
+        //         postData
+        //     }
+        // }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    display: flex;
+    padding: 10px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: stretch;
+    justify-content: center;
+    flex-direction: row-reverse;
+    width: 100%;
+    // background: crimson;
+    // min-height: 100%;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+    }
+  }
+</style>
---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter5-connecting_to_backend/pages/admin/new-post/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="admin-new-post-page">
+      <section class="new-post">
+          <AdminPostForm @addingPost="addPost"></AdminPostForm>
+      </section>
+  </div>
+</template>
+
+<script>
+
+import AdminPostForm from "~/components/Admin/AdminPostForm";
+import axios from 'axios';
+
+export default {
+    layout: 'adminLayout',
+    components: {
+        AdminPostForm,
+    },
+    methods: {        
+        addPost(postData) {
+
+            axios.post('https://nuxtblogproject.firebaseio.com/post.json', postData)
+            .then(result => console.log(result))
+            .catch(e => console.log(e))
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+.new-post {
+    width: 90%;
+    margin: 2rem auto;
+
+    @media (min-width: 769px) {
+        width: 50rem;
+    }
+}
+
+</style>
