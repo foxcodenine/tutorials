8b96eaa8b0c9d12869f75fb06477448fb88ec21a
foxcodenine <foxcode9@gmail.com>
2020-10-26 07:30:32 +0100

    started chapter5 - add firebase backend

---
+++ udemy_courses/nuxt-js_vue-js_on_steroids/chapter5-connecting_to_backend/pages/admin/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="admin-page">
+
+        <section class="new-post">
+            <AppButton                 
+                @click="onClick()"
+                btn-style="blue">Create Post
+            </AppButton>
+        </section>
+
+        <section class="existing-post">
+            <h1 class="existing-post__title">Existing Post</h1>
+            <PostSection isAdmin :dataPost="dataPost"></PostSection>
+        </section>
+  </div>
+</template>
+
+
+
+<script>
+    import PostSection from "~/components/Post/PostSection";
+    import AppButton from "~/components/UI/AppButton";
+    export default {
+        layout: 'adminLayout',
+        components: {
+            PostSection,
+            AppButton
+        },
+    // _________________________________________________________________
+
+    // Superseded code - Using asyncData instaed
+    // Superseded code - Using fetch and retrive data from store with getter in computed
+
+        // data() {
+        //     return {
+        //         dataPost: []
+        //     }
+        // },
+        // created() {
+        //     fetch('http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}})
+        //     .then(res => res.json())
+        //     .then(data => this.dataPost = [...data].reverse())
+        // },
+    // _________________________________________________________________
+
+    
+        fetch(context) {
+            return fetch(
+                'http://127.0.0.1:5000/nuxtAPI/', {headers: {'API-Nuxt-Key': '123#456#789'}}
+                )
+                .then(res => res.json())
+                .then(data => {
+                    context.store.dispatch('setPost', data.reverse())
+                }
+            )
+        },
+        computed: {
+            dataPost() {
+                return this.$store.getters.fetchPost
+            }
+        },
+        methods: {
+            onClick() {
+                this.$router.push('/admin/new-post')
+            }
+        },
+
+    }
+</script>
+
+
+
+<style lang="scss" scoped>
+    .admin-page {
+        padding: 2rem;
+    }
+    
+    .new-post {
+        text-align: center;
+        border-bottom: 2px solid #ccc;
+        padding-bottom: 1rem;
+    }
+    
+    .existing-post {
+
+        &__title {
+            text-align: center;
+        }
+    }
+</style>
