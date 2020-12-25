7c2e20bb9be0f7d9e3b9c92e5120438edde21f9e
Chris <foxcode9@gmail.com>
2020-12-25 08:10:03 +0100

    deleteing pictures from db and aws

--- udemy_courses/nuxt-js_vue-js_on_steroids/projects/trava/frontend_nuxt/store/modules/pictures.js
+++ udemy_courses/nuxt-js_vue-js_on_steroids/projects/trava/frontend_nuxt/store/modules/pictures.js
@@ -1,3 +1,5 @@
+import { slice } from "core-js/fn/array";
+
 const actions = {
 
     addPicture(vuexContext, payload) {
@@ -10,24 +12,46 @@ const actions = {
         })
     },
 
-    async fetchUserBoxes({commit, dispatch, rootGetters}) {
+    async deletePicture({rootGetters}) {
+
+        const token = rootGetters.getToken;
+
+        // Selecting only the image id from the image url
+        let toDelete = rootGetters.getBoxToDelete;        
+        toDelete = toDelete.map(i => i.split('.').slice(-2, -1));
+        toDelete = toDelete.map(i => i[0].split('/').slice(-1)[0]);
+
+        const payload = {toDelete, token};
+        // console.log(payload);
+        
+        try {
+            const response = await this.$axios.$post('trava/pictures/deletePicture/', payload);
+            console.log(response);
+            return response;
+
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    async fetchUserBoxes({commit, dispatch, rootGetters, rootState}) {
         const token = await rootGetters.getToken;
 
         try {
             const respons =  await this.$axios.$post(
                 'trava/pictures/fetchPictures/', {token});
 
-
-
             const userPictures = respons.user_pictures;
+
+            
+
             return userPictures;
 
         } catch(err) {
             console.log(err);
-        }
-
-        
+        }        
     },
+    
 }
 
 export default {
