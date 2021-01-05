1dc31df255de26d27501674b0f998cc000daf254
Chris Farrugia <foxcode9@gmail.com>
2021-01-05 08:03:05 +0100

    completed chapter9 - php_and_the_web

--- udemy_courses/PHP_for_Beginners/demo/09/cookies.php
+++ udemy_courses/PHP_for_Beginners/demo/09/cookies.php
@@ -4,12 +4,12 @@ $name = "SomeName";
 $data = ['code' => 200, 'value' => 'some_value'];
 $exp = time() + (60*60*24*7);
 
-$value = serialize($value);
+$value = serialize($data);
 // Generates a storable representation of a value. (to save an array in cookie)
 // This is useful for storing or passing PHP values around without losing their type and structure. 
 // Then unserialize() data
 
-setcookie($name, $data, $exp);
+setcookie($name, $value, $exp);
 
 ?>
 
@@ -22,7 +22,25 @@ setcookie($name, $data, $exp);
 </head>
 <body>
 
+<?php
 
+if (isset($_COOKIE['SomeName'])) {
+    echo 'cookie is set!' . '<br>';
+
+    // fectch cookie data and echo
+    $some_name = $_COOKIE['SomeName'];
+    echo $some_name . '<br>';
+
+    // unsterialize data and print_r
+    $some_name = unserialize($some_name);
+    print_r($some_name) ;
+
+} else {
+    echo 'cookie is NOT set!';
+    $some_name = '';
+}
+
+?>
 
 </body>
 </html>
