ce21776339c55f4868665bff8e3a96c356055edb
Chris Farrugia <foxcode9@gmail.com>
2021-01-19 07:08:31 +0100

    update to cms project

--- udemy_courses/PHP_for_Beginners/cms/admin/categories.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/categories.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include './includes/header.php'?>
-<?php require_once '../includes/db.php'?>
+<?php include './includes/admin_header.php'?>
 
 <body>
 
     <div id="wrapper">
 
         <!-- Navigation -->
-        <?php include './includes/nav_admin.php'?>
+        <?php include './includes/admin_nav.php'?>
 <!-- --------------------------------------------------------------- -->
 
         <div id="page-wrapper">
@@ -26,7 +25,65 @@
                         </h1>
 
                         <div class="col-xs-6">
-                        <form action="">
+
+                        <?php 
+                        // _____________________________________________
+                        // ______ Fetch Categories from db
+
+                        $sql = 'SELECT * FROM cms_categories;';
+                        $categories = $conn->query($sql);                                     
+
+                        // _____________________________________________
+                        ?>
+                        <?php 
+                        // _____________________________________________
+                        // ______ New Add Categories
+
+                        if (isset($_POST['submit'])) {
+
+                            $cat_name_title = $_POST['cat_title'];
+                            
+                            if (trim($cat_name_title) === '' || empty($cat_name_title)) {
+
+                                echo '<h5 style="color: crimson;">This field should not be empty! </h5>';
+
+                            } else {    
+
+                                $sql = "INSERT INTO cms_categories (cat_title) VALUES ('{$cat_name_title}');";
+
+                                if ($conn->query($sql) !== TRUE) {
+                                    echo "Error: " . "<br>" . $conn->error;
+                                } else {
+                                    echo "<meta http-equiv='refresh' content='0'>";                                    
+                                }
+                            }
+                        } 
+                                                
+                                                
+                        // _____________________________________________                      
+                        ?>
+                        <?php
+                        // _____________________________________________
+                        // Deleteing Category
+                        
+                        if (isset($_GET['delete'])) {
+
+                            $delete_cat_id = $_GET['delete'];
+
+                            $sql = "DELETE FROM cms_categories WHERE cat_id = '{$delete_cat_id}'";
+
+                            if ($conn->query($sql) !== TRUE) {
+                                echo 'Error:' . '<br>' . $conn->error;
+                            } else {
+                                // echo "<meta http-equiv='refresh' content='0'>";  // <- to refresh
+                                header("Location: categories.php");                 // <- to refresh
+                            }
+                        }
+                        
+                        // _____________________________________________  
+                        ?>
+
+                        <form action="?" method="POST">
                             <div class="form-group">
                             <label for="cat-title">Add Categories</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -35,7 +92,47 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+
+                        <?php 
+                        // Update category
+                        if (isset($_GET['edit'])) {
+
+                            
+                            $sql = "SELECT * FROM cms_categories WHERE cat_id = '{$_GET['edit']}' LIMIT 1;";
+
+                            $edit_cat = $conn->query($sql);
+
+                            if ($conn->error) {
+                                die('Error: ' . '<br>' . $conn->error);
+                            } else {
+                                $edit_cat = mysqli_fetch_assoc($edit_cat);
+                            }
+
+                        }
+                        
+                        ?>
+
+                        <form action="?" method="POST">
+                            <div class="form-group">
+                            <label for="cat-title">Add Categories</label>
+                                <input class="form-control" type="text" name="cat_title"
+                                    <?php
+                                    if (isset($edit_cat)) {
+                                        echo "value='{$edit_cat['cat_title']}'";
+                                    }
+                                    
+                                    ?>                                
+                                >
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="update" value="Update">
+                            </div>
+                        </form>
+
+
                         </div>
+
+
 
                         <div class="col-xs-6">
                             <table class="table table-bordered table-hover">
@@ -43,17 +140,35 @@
                                     <tr>
                                         <th>Id</th>
                                         <th>Category Title</th>
+                                        <th></th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Baseball Category</td>
-                                        <td>Basketball Category</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Baseball Category</td>
-                                        <td>Basketball Category</td>
-                                    </tr>
+
+                                <?php
+                                // _____________________________________ 
+
+                                // echo Categories
+
+                                if (isset($categories) && $categories->num_rows > 0) {
+
+                                    while($row = mysqli_fetch_assoc($categories)) {
+
+                                        $cat_id = $row['cat_id'];
+                                        $cat_title = $row['cat_title'];
+
+                                                            echo "<tr>";
+                                                            echo "<td>{$cat_id}</td>";
+                                                            echo "<td>{$cat_title}</td>";
+                                                            echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                                            echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+                                                            echo "</tr>";                                            
+                                    }
+                                }               
+                                // _____________________________________ 
+                                ?>
+
                                 </tbody>
                             </table>
                         </div>
@@ -71,7 +186,7 @@
     </div>
     <!-- /#wrapper -->
 
-    <?php require 'includes/scripts.php'; ?>
+    <?php require 'includes/admin_scripts.php'; ?>
 
 </body>
 
