8d0b51dc24b0d587d63a5d941da3c62323ed085a
chris <foxcode9@gmail.com>
2021-01-24 10:53:18 +0100

    add post_delete and started post_edit

--- udemy_courses/PHP_for_Beginners/cms/admin/includes/view_all_post.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/includes/view_all_post.php
@@ -1,7 +1,72 @@
+<!-- --------------------------------------------------------------- -->
 <?php
+
+// ---- Fetch all post from database and return
+
         $sql = "SELECT * FROM cms_posts;";
         $result_post = $conn->query($sql);
 ?>
+<!-- --------------------------------------------------------------- -->
+
+<?php 
+// ---- Delete post
+
+        if (isset($_GET['delete']) && trim($_GET['delete']) !== '') {
+
+
+            // _________________________________________________________
+
+            // ----- getting image path 
+
+            $sql = "SELECT post_image FROM cms_posts WHERE post_id = {$_GET['delete']};";
+            $image_object = $conn->query($sql);
+
+            
+            if(isset($image_object) && mysqli_num_rows($image_object) > 0) {
+
+                // 1. converting to an url string
+                $image_url = $image_object->fetch_assoc()['post_image'];
+
+                // 2. splitting string to array
+                $image_array = explode('/', $image_url);
+
+                // 3. selecting image name from array
+                echo $image_name = end($image_array);
+
+                echo '<br>';
+
+                // 4. retuning absolute path to image
+                echo $image_path = dirname(dirname(__DIR__)) . "/images/{$image_name}";
+                
+                
+            } else {
+
+                die('Error! post_id not found in db');
+            }
+
+            // _________________________________________________________
+            
+            // Deleting record from database 
+            $sql = "DELETE FROM cms_posts WHERE post_id = {$_GET['delete']}";
+
+ 
+            if ($conn->query($sql) !== TRUE) {
+                
+                die("Error deleting record: " . $conn->error); 
+
+            } else {
+
+            // Deleting image file and refresh page
+            unlink($image_path);
+            header('Location: '. $_SERVER['PHP_SELF']);
+            
+            }
+
+        }
+
+?>
+
+<!-- --------------------------------------------------------------- -->
 
 <table class="table table-bordered table-hover">
     <thead >
@@ -15,6 +80,8 @@
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th></th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
@@ -39,10 +106,12 @@
             echo "<td>{$post_title}</td>";
             echo "<td>{$post_cat_id}</td>";
             echo "<td>{$post_statas}</td>";
-            echo "<td><img style='width: 200px;' src='.{$post_image}'></td>";
+            echo "<td><img style='width: 200px;' src='{$post_image}'></td>";
             echo "<td>{$post_tags}</td>";
             echo "<td>{$post_count}</td>";
             echo "<td>{$post_date}</td>";
+            echo "<td><a href='{$_SERVER['PHP_SELF']}?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+            echo "<td><a href='{$_SERVER['PHP_SELF']}?delete={$post_id}'>Delete</a></td>";
             echo "</tr>";
         }
     ?>
