1b386a5093e66f27f99a0775ecc4c44298ce152d
Christopher Farrugia <foxcode9@gmail.com>
2021-02-09 10:21:46 +0100

    complete chapter 18 Profile

--- udemy_courses/PHP_for_Beginners/cms/admin/includes/admin_nav_top.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/includes/admin_nav_top.php
@@ -79,12 +79,16 @@
     </li> -->
 
     <!-- --------------------------------------------------------------- -->
+    <?php $fullname = $_SESSION['firstname'] . ' ' . $_SESSION['lastname'];?>
 
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-user"></i> <?php echo $fullname; ?> <b class="caret"></b>
+        </a>
         <ul class="dropdown-menu">
 
-            <li><a href="#"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+            <li><a href="./profile.php"><i class="fa fa-fw fa-user"></i> Profile </a></li>
+
             <!-- <li><a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a></li> -->
             <!-- <li><a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a></li> -->
             <li class="divider"></li>
