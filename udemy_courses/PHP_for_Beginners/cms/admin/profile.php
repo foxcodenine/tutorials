9f045f83b08c274e470a7231a06343034eeb0828
Chris Farrugia <foxcode9@gmail.com>
2021-02-09 10:21:46 +0100

    complete chapter 18 Profile

--- udemy_courses/PHP_for_Beginners/cms/admin/profile.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/profile.php
@@ -1,30 +1,64 @@
-<?php admin_add_user();?>
+<!DOCTYPE html>
+<html lang="en">
 
+<?php include './includes/admin_header.php'?>
 
+<body>
+
+    <div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include './includes/admin_nav.php'?>
+<!-- --------------------------------------------------------------- -->
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            
+            <div class="row">
+                <div class="col-lg-12">
+
+                <h1 class="page-header">
+                    Welcome To Admin
+                    <small>Users</small>
+                </h1>
+
+<!-- -------------------------------------- -->
+<?php     
+
+    if (isset($_SESSION['username'])) {
+        $current_username = $_SESSION['username'];
+
+        $sql = "SELECT * FROM cms_users WHERE user_username = '{$current_username}';";
+
+        $result = $conn->query($sql);
+
+        if ($conn->error) {
+            die('Error: ' . '<br>' . $conn->error);
+        }
+
+        while ($row = $result->fetch_assoc()) {
+            $user_id = $row['user_id'];
+            $user_username = $row['user_username'];
+            $user_password = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+            $user_image = $row['user_image'];
+            $user_role = $row['user_role'];
+            $user_date = $row['user_date'];
+            $user_randsalt = $row['user_randsalt'];
+        }
+
+    }
+
+?>
+
+<!-- -------------------------------------- -->
 
 <?php
-$current_user_id = $_GET['u_id'];
-
-$form_action = $_SERVER['PHP_SELF'] . "?source=edit_user&u_id={$current_user_id }"; 
-
-$sql = "SELECT * FROM cms_users WHERE user_id = {$current_user_id}";
-
-$result = $conn->query($sql);
-
-if ($conn->error) {
-    die('Error: ' . '<br>') . $conn->error;
-}
-
-while($row = $result->fetch_assoc()) {
-    $user_firstname = $row['user_firstname'];
-    $user_lastname = $row['user_lastname'];
-    $user_role = $row['user_role'];
-    $user_username = $row['user_username'];
-    $user_email = $row['user_email'];
-    $user_password = $row['user_password'];
-    $user_date = $row['user_date'];
-}
-
 
 if (isset($_POST['edit_user'])) {
 
@@ -45,7 +79,7 @@ if (isset($_POST['edit_user'])) {
             user_password = '{$user_password}',
             user_date = '{$user_date}'
             
-            WHERE user_id = '{$current_user_id}'";
+            WHERE user_username = '{$_SESSION['username']}'";
 
     $conn->query($sql);
 
@@ -54,13 +88,13 @@ if (isset($_POST['edit_user'])) {
     } else {
         header('Location: '. $_SERVER['PHP_SELF']);
     }
-}
-
+} 
 ?>
 
-<h2>Edit User</h2>
+<!-- -------------------------------------- -->
+<h2>Profile Page</h2>
 
-<form action="<?php echo $form_action ?>" method="post" enctype="multipart/form-data">
+<form action="./profile.php?send=yes" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="user_firstname">Firstname</label>
@@ -79,17 +113,7 @@ if (isset($_POST['edit_user'])) {
 
 <!-- --------------------------------------------------------------- -->
         <?php
-            // $sql = "SELECT * FROM cms_categories;";
 
-            // $result = $conn->query($sql);
-            
-            // if($result != TRUE) {
-            //     die('Error:' . '<br>' . $conn->error);
-            // }
-
-            // while ($row = $result->fetch_assoc()) {            
-            //     echo "<option  value='{$row['cat_id']}'>{$row['cat_title']}</option>";
-            // }
             $roles = ['Admin', 'Subscriber'];
             
             foreach ($roles as $role) {               
@@ -106,8 +130,6 @@ if (isset($_POST['edit_user'])) {
 <!-- --------------------------------------------------------------- -->
     </select>
     </div>
-
-
 
     <div class="form-group">
         <label for="user_username">Username</label>
@@ -133,7 +155,27 @@ if (isset($_POST['edit_user'])) {
 
 
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="edit_user" value="Update User">
+        <input type="submit" class="btn btn-primary" name="edit_user" value="Update Profile">
     </div>
 
-</form>
+
+</form>      
+<!-- -------------------------------------- -->
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+<!-- --------------------------------------------------------------- -->
+    </div>
+    <!-- /#wrapper -->
+
+    <?php require 'includes/admin_scripts.php'; ?>
+
+</body>
+
+</html>
