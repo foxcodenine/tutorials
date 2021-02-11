84d3f595be3b92f86f4ebcd779c05be585c26279
Christopher Farrugia <foxcode9@gmail.com>
2021-02-11 07:55:29 +0100

    started chapter19 - Dashboard

--- udemy_courses/PHP_for_Beginners/cms/admin/index.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/index.php
@@ -40,15 +40,186 @@
                     </div>
                 </div>
                 <!-- /.row -->
+                                <!-- /.row -->
+                
+<div class="row">
 
+    <?php 
+
+    // __________________________________
+    
+    $sql = "SELECT * FROM cms_posts;";
+    $result = $conn->query($sql);
+
+    if ($conn->error) {die('Error Post: ' . '<br>' . $conn->error); }
+    
+    $post_count = $result->num_rows;
+
+    // __________________________________
+
+    $sql = "SELECT * FROM cms_comments;";
+    $result = $conn->query($sql);
+
+    if ($conn->error) {die('Error Post: ' . '<br>' . $conn->error); }
+    
+    $comments_count = $result->num_rows;
+
+    // __________________________________
+
+    $sql = "SELECT * FROM cms_users;";
+    $result = $conn->query($sql);
+
+    if ($conn->error) {die('Error Post: ' . '<br>' . $conn->error); }
+    
+    $users_count = $result->num_rows;
+
+    // __________________________________
+
+    $sql = "SELECT * FROM cms_categories;";
+    $result = $conn->query($sql);
+
+    if ($conn->error) {die('Error Post: ' . '<br>' . $conn->error); }
+    
+    $categories_count = $result->num_rows;
+
+    // __________________________________
+    ?>
+
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file-text fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                  <div class='huge'><?php echo $post_count;?></div>
+                        <div>Posts</div>
+                    </div>
+                </div>
+            </div>
+            <a href="./post.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-comments fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                     <div class='huge'><?php echo $comments_count;?></div>
+                      <div>Comments</div>
+                    </div>
+                </div>
+            </div>
+            <a href="comments.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                    <div class='huge'><?php echo $users_count;?></div>
+                        <div> Users</div>
+                    </div>
+                </div>
+            </div>
+            <a href="users.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-list fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class='huge'><?php echo $categories_count;?></div>
+                         <div>Categories</div>
+                    </div>
+                </div>
+            </div>
+            <a href="categories.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+<!-- --------------------------------------------------------------- -->
+
+<div class="row">
+<script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Data', 'Count'],
+
+        <?php ?>
+
+          ['Posts', 1000]
+        ]);
+
+        var options = {
+          chart: {
+            title: '',
+            subtitle: '',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+</script>
+<div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+
+</div>
+<!-- --------------------------------------------------------------- -->
+                <!-- /.row -->
             </div>
             <!-- /.container-fluid -->
 
         </div>
         <!-- /#page-wrapper -->
+        
 <!-- --------------------------------------------------------------- -->
     </div>
     <!-- /#wrapper -->
+    
 
     <?php require 'includes/admin_scripts.php'; ?>
 
