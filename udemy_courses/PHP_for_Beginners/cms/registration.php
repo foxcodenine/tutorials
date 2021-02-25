690999531a0552ee6357cce7ee4709acd3b9e515
Christopher Farrugia <foxcode9@gmail.com>
2021-02-25 10:59:14 +0100

    start working on registration page

---
+++ udemy_courses/PHP_for_Beginners/cms/registration.php
@@ -0,0 +1,79 @@
+
+<?php  include "includes/db.php"; ?>
+
+<?php 
+
+if (isset($_POST['submit_reg'])) {
+
+    $email_reg    = htmlspecialchars($_POST['email_reg']);    
+    $username_reg = htmlspecialchars($_POST['username_reg']);
+    $password_reg = htmlspecialchars($_POST['password_reg']);
+
+    $email_reg    = $conn->real_escape_string($email_reg);    
+    $username_reg = $conn->real_escape_string($username_reg);
+    $password_reg = $conn->real_escape_string($password_reg);
+
+
+
+    echo "{$email_reg} {$username_reg} {$password_reg}";
+    
+}
+
+
+?>
+
+<!-- --------------------------------------------------------------- -->
+<!DOCTYPE html>
+<html lang="en">
+
+<?php  include "includes/header.php"; ?>
+
+<body>
+
+
+<!-- Navigation -->
+
+<?php  include "includes/navigation.php"; ?>
+
+
+<!-- Page Content -->
+<div class="container" style="margin-top:10vh">
+    
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+                <div class="form-wrap">
+                <h1>Register</h1>
+                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                        <div class="form-group">
+                            <label for="username" class="sr-only">username</label>
+                            <input type="text" name="username_reg" id="username" class="form-control" placeholder="Enter Desired Username">
+                        </div>
+                         <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email_reg" id="email" class="form-control" placeholder="somebody@example.com">
+                        </div>
+                         <div class="form-group">
+                            <label for="password" class="sr-only">Password</label>
+                            <input type="password" name="password_reg" id="key" class="form-control" placeholder="Password">
+                        </div>
+                
+                        <input type="submit" name="submit_reg" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                    </form>
+                 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
+
+
+<hr style="margin-top:40vh">
+
+
+
+<?php include "includes/footer.php";?>
+
+</body>
+</html>
