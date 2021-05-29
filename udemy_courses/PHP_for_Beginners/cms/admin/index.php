ad05ee2efea1c1f99a4a605de2f9e9bcf513ddd7
Chris Farrugia <foxcode9@gmail.com>
2021-05-29 11:21:09 +0200

    added unlike to post page

--- udemy_courses/PHP_for_Beginners/cms/admin/index.php
+++ udemy_courses/PHP_for_Beginners/cms/admin/index.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Welcome To Admin
+                            Welcome To Admin 
                             <small><?php echo  $_SESSION['firstname'] . ' ' . $_SESSION['lastname']; ?></small>
 
                             
--- udemy_courses/PHP_for_Beginners/cms/includes/scripts.php
+++ udemy_courses/PHP_for_Beginners/cms/includes/scripts.php
@@ -1,9 +1,9 @@
-jQuery
-    <script src="http://localhost/htdocs/cms/js/jquery.js"></script>
+<!-- jQuery -->
+<script src="http://localhost/htdocs/cms/js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="http://localhost/htdocs/cms/js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="http://localhost/htdocs/cms/js/bootstrap.min.js"></script>
 
 
-    <!-- js-->
-    <script src="http://localhost/htdocs/cms/admin/js/scripts.js"></script>
+<!-- js-->
+<script src="http://localhost/htdocs/cms/admin/js/scripts.js"></script>
