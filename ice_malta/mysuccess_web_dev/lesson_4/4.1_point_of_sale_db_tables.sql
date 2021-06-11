98c1025a34a9857268f135de62a9968549926c5a
Christopher Farrugia <54644062+foxcodenine@users.noreply.github.com>
2021-06-11 08:49:39 +0200

    adjusting tab spacing

--- ice_malta/mysuccess_web_dev/lesson_4/4.1_point_of_sale_db_tables.sql
+++ ice_malta/mysuccess_web_dev/lesson_4/4.1_point_of_sale_db_tables.sql
@@ -8,46 +8,46 @@ USE point_of_sale;
 
 -- 1
 CREATE TABLE Cashier(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName 	VARCHAR(30) NOT NULL,
-    lastName  	VARCHAR(30) NOT NULL,
-    isActive 	BOOL DEFAULT true
+	id 		INT AUTO_INCREMENT PRIMARY KEY,
+    	firstName 	VARCHAR(30) NOT NULL,
+    	lastName  	VARCHAR(30) NOT NULL,
+    	isActive 	BOOL DEFAULT true
 );
 
 
 -- 2
 CREATE TABLE Client(
-	id 			INT AUTO_INCREMENT PRIMARY KEY,
-    firstName 	VARCHAR(30) NOT NULL,
-    lastName  	VARCHAR(30) NOT NULL,
-    phone		INT,
-    email 		VARCHAR(30) NOT NULL,
-    isBlocked 	BOOL DEFAULT false
+	id 		INT AUTO_INCREMENT PRIMARY KEY,
+    	firstName 	VARCHAR(30) NOT NULL,
+    	lastName  	VARCHAR(30) NOT NULL,
+    	phone		INT,
+    	email 		VARCHAR(30) NOT NULL,
+    	isBlocked 	BOOL DEFAULT false
 );
 
 
 -- 3
 CREATE TABLE PayType(
-	id 			INT AUTO_INCREMENT PRIMARY KEY,
-    typeName 	VARCHAR(30) NOT NULL
+	id 		INT AUTO_INCREMENT PRIMARY KEY,
+    	typeName 	VARCHAR(30) NOT NULL
 );
 
 
 -- 4
 CREATE TABLE Category(
-	id 			INT AUTO_INCREMENT PRIMARY KEY,
-    catName 	VARCHAR(30) NOT NULL
+	id 		INT AUTO_INCREMENT PRIMARY KEY,
+    	catName 	VARCHAR(30) NOT NULL
 );
 
 -- 5
 CREATE TABLE Payment(
-	id 			 	INT AUTO_INCREMENT PRIMARY KEY,
-    dateAndTime		DATETIME DEFAULT CURRENT_TIMESTAMP,
-    total			DECIMAL(8,2),
-    clientId		INT NOT NULL,
-    payTypeId		INT NOT NULL,
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+    	dateAndTime		DATETIME DEFAULT CURRENT_TIMESTAMP,
+    	total			DECIMAL(8,2),
+    	clientId		INT NOT NULL,
+    	payTypeId		INT NOT NULL,
     
-    CONSTRAINT 	c_payment_client
+    	CONSTRAINT 	c_payment_client
 		FOREIGN KEY(clientId) REFERENCES Client(id),        
 	CONSTRAINT 	c_payment_paytype
 		FOREIGN KEY(payTypeId) REFERENCES PayType(id)
@@ -55,12 +55,12 @@ CREATE TABLE Payment(
 
 -- 6
 CREATE TABLE Item(
-	id 		 	INT AUTO_INCREMENT PRIMARY KEY,
+	id 		INT AUTO_INCREMENT PRIMARY KEY,
 	itemName	VARCHAR(20) NOT NULL,
-    price		DECIMAL(8, 2) NOT NULL,
-    stockUnit	VARCHAR(20) DEFAULT 'not_applicable',
-    stockQty	INT NOT NULL,
-    categoryId	INT NOT NULL,
+    	price		DECIMAL(8, 2) NOT NULL,
+    	stockUnit	VARCHAR(20) DEFAULT 'not_applicable',
+    	stockQty	INT NOT NULL,
+    	categoryId	INT NOT NULL,
 	CONSTRAINT 	c_item_category
 		FOREIGN KEY(categoryId) REFERENCES Category(id)
 );
@@ -71,25 +71,25 @@ ALTER TABLE Item
 
 -- 7
 CREATE TABLE Invoice(
-		id 			 	INT AUTO_INCREMENT PRIMARY KEY,
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
         dateAndTime		DATETIME DEFAULT CURRENT_TIMESTAMP,
         reversed		BOOL DEFAULT false,
-		total			DECIMAL(8, 2) NOT NULL,
+	total			DECIMAL(8, 2) NOT NULL,
         cashierId		INT NOT NULL,
         clientId		INT NOT NULL,
         
-		CONSTRAINT 	c_invoice_cashier
-			FOREIGN KEY(cashierId) REFERENCES Cashier(id),            
-		CONSTRAINT 	c_invoice_client
-			FOREIGN KEY(clientId) REFERENCES Client(id)       
+	CONSTRAINT 	c_invoice_cashier
+		FOREIGN KEY(cashierId) REFERENCES Cashier(id),            
+	CONSTRAINT 	c_invoice_client
+		FOREIGN KEY(clientId) REFERENCES Client(id)       
 );
 
 
 -- 8 
 CREATE TABLE PaymentInvoice(
-	id 			 	INT AUTO_INCREMENT PRIMARY KEY,    
-    invoiceId		INT NOT NULL,
-    paymentId		INT NOT NULL,
+	id 			INT AUTO_INCREMENT PRIMARY KEY,    
+    	invoiceId		INT NOT NULL,
+    	paymentId		INT NOT NULL,
 	
 	CONSTRAINT 	c_paymentinvoice_invoice
 		FOREIGN KEY(invoiceId) REFERENCES Invoice(id),
@@ -100,11 +100,11 @@ CREATE TABLE PaymentInvoice(
 
 -- 9  
 CREATE TABLE InvoiceItem(
-	id 			 	INT AUTO_INCREMENT PRIMARY KEY,      
-    itemUnit		VARCHAR(20) DEFAULT 'not_applicable',
-    itemQty			INT NOT NULL,
-    invoiceId		INT NOT NULL,
-    itemId			INT NOT NULL,    
+	id 		INT AUTO_INCREMENT PRIMARY KEY,      
+    	itemUnit	VARCHAR(20) DEFAULT 'not_applicable',
+    	itemQty		INT NOT NULL,
+    	invoiceId	INT NOT NULL,
+    	itemId		INT NOT NULL,    
     
 	CONSTRAINT 	c_invoiceitem_item
 		FOREIGN KEY(itemId) REFERENCES Item(id),
@@ -114,10 +114,10 @@ CREATE TABLE InvoiceItem(
 
 -- 10
 CREATE TABLE Alocation(
-	id 			 		INT AUTO_INCREMENT PRIMARY KEY, 
-    itemQty				INT NOT NULL,
-    paymentInvoiceId	INT NOT NULL,
-    invoiceItemId		INT NOT NULL,
+	id 			INT AUTO_INCREMENT PRIMARY KEY, 
+    	itemQty			INT NOT NULL,
+    	paymentInvoiceId	INT NOT NULL,
+    	invoiceItemId		INT NOT NULL,
     
 	CONSTRAINT 	c_alocation_paymentinvoice
 		FOREIGN KEY(paymentInvoiceId) REFERENCES PaymentInvoice(id)
