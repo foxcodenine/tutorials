5824272e4411fe714a4ac8aeea75c241e873870c
Chris Farrugia <foxcode9@gmail.com>
2021-06-27 21:47:28 +0200

    complete slides for lesson 5

---
+++ ice_malta/mysuccess_web_dev/PHP/lesson_5/test_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ice Malta PHP course - Lesson 5">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="HTML, CSS, PHP, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/css/style.css">
+    <title>PHP Lesson 5</title>
+</head>
+<body>
+    <div class="container">
+
+        <h2>MySuccess Website Developer - Lesson 5 Test Page</h2>
+
+        
+        
+    </div>
+</body>
+</html>
