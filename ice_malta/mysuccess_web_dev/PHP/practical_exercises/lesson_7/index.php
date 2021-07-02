35f53a68d240b3f8f79d07ac5b09e144cdd759d2
foxcodenine <foxcode9@gmail.com>
2021-07-02 07:18:37 +0200

    complete php practical 7

--- ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_7/index.php
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_7/index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./static/css/style.css">
-    <title>PHP Lesson 6</title>
+    <title>PHP Lesson 7</title>
 </head>
 <body>
     <div class="container">
 
-        <h2>MySuccess Website Developer - PHP Lesson 6</h2>
+        <h2>MySuccess Website Developer - PHP Lesson 7</h2>
 
         <?php include "./questions.php"; ?>
         
