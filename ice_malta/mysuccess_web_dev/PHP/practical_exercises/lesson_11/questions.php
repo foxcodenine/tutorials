14ea53b581e5dfce90f21a0178178c8d409b0dd9
Chris Farrugia <foxcode9@gmail.com>
2021-07-15 22:54:49 +0200

    start lesson11 practical

---
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_11/questions.php
@@ -0,0 +1,54 @@
+
+<!-- helper functions ---------------------------------------------- -->
+<section class="exercise">      
+   <div class="left">
+    <div class="left__title">discover the icelandic turf house</div>   
+    <form action="" class="left__form">
+
+        <label for="firstname" class="left__label">firstname</label>
+        <input type="text" class="left__input" name="firstname" id="firstname">
+
+        <label for="lastname" class="left__label">lastname</label>
+        <input type="text" class="left__input" name="lastname" id="lastname">
+
+        <label for="email" class="left__label">email</label>
+        <input type="text" class="left__input" name="email" id="email">
+
+        <label for="check_in" class="left__label">check in</label>
+        <input type="date" class="left__input" name="check_in" id="check_in">
+
+        <label for="check_out" class="left__label">check out</label>
+        <input type="date" class="left__input" name="check_out" id="check_out">
+
+        <div class="left__room">
+            <input type="text" class="left__input left__input--adults" placeholder="adults" name="adults">
+            <input type="text" class="left__input left__input--children" placeholder="children" name="children">
+            <input type="text" class="left__input left__input--room" placeholder="room" name="room">
+        </div>
+
+        <label for="check_out" class="left__label">select color</label>
+        <!-- <input type="date" class="left__input" name="check_out" id="check_out"> -->
+        
+        <select name="bg_color" class="left__input" >
+            <option style="background-color: #f3f3f8;" value="f3f3f8">sky</option>
+            <option style="background-color: #FFF;" value="#FFF">white</option>
+            <option style="background-color: #FEE1D1;" value="#FEE1D1">orange</option>
+            <option style="background-color: #E0FFFF;" value="#E0FFFF">cyan</option>
+            <option style="background-color: #FAFAD2;" value="#FAFAD2">yellow</option>
+            <option style="background-color: #E6E6FA" value="#E6E6FA">lavender</option>
+            <option style="background-color: #D1D4D0" value="#D1D4D0">gray</option>
+            <option style="background-color: #FFF0F5;" value="#FFF0F5">pink</option>
+            <option style="background-color: #d2ffe1;" value="#d2ffe1">mint</option>
+            <option style="background-color: #F0D8CC;" value="#F0D8CC">brown</option>
+
+        </select>
+
+        <button  class="left__btn">Reserve</button>
+    </form>
+
+
+   </div>
+
+</section>
+
+<!-- --------------------------------------------------------------- -->
