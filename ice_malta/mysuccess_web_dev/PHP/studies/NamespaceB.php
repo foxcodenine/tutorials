34a11150f11c2a7d6281ebf905703dd98250ebda
Christopher Farrugia <foxcode9@gmail.com>
2021-08-01 13:57:49 +0200

    updateds

---
+++ ice_malta/mysuccess_web_dev/PHP/studies/NamespaceB.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace NamespaceB;
+
+
+class BB {
+    
+    public function __construct() {
+        echo '<br>' . __CLASS__ . '<br>';
+    }
+}
+
+
+
+const BBB = 'BBB';
+
+
+
+
+function bbbb() {
+    echo 'bbbb';
+}
