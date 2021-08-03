261084449e19d88da3fb01de97ab1f8598a64e93
Chris Farrugia <foxcode9@gmail.com>
2021-08-03 21:20:26 +0200

    start_practical_12

---
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/app/Model/Rabbit.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Model;
+
+class Rabbit {
+    public function __construct()
+    {
+        echo '<br>A rabbit was born!</br>';
+    }
+}
---
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/css/style.css">
+    <link rel="stylesheet" href="/ice_malta/php/header/static/css/style.css">
+    <title>PHP Lesson 12</title>
+    <?php
+        require_once './app/init.php';
+    ?>
+
+</head>
+
+<body>
+<div class="container"> 
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/ice_malta/php/header/index.php'?>
+    <h2>MySuccess Website Developer - PHP Lesson 12</h2>
+    
+    <?php 
+    
+    
+    var_dump($_ENV);
+    
+    
+    ?>
+
+
+
+</div>
+</body>
+</html>
