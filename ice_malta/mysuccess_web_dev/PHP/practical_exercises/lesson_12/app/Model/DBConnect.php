fb224c8810c5d1d70d1fc9b05756cf7aa3bd88da
Chris Farrugia <foxcode9@gmail.com>
2021-08-05 21:48:06 +0200

    updateds

--- ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/app/Model/DBConnect.php
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/app/Model/DBConnect.php
@@ -9,29 +9,63 @@ class DBConnect {
 
 
     private static $conn = null;
+    private static $db_type;
+    private static $db_host;
+    private static $db_schema;
+    private static $db_username;
+    private static $db_password;
+    
+    // _________________________________________
 
     public static function getConnection() {
+        if(self::$conn === null) {
 
-
-        try {
-            if(self::$conn === null) {
+            self::loadENV();
+            
+            try {                
                 $conn = new PDO(
-                    $_ENV['DB_TYPE'] . ':host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_SCHEMA'],
-                    $_ENV['DB_USERNAME'],
-                    $_ENV['DB_PASSWORD']
+                    self::$db_type . ':host=' . self::$db_host . ';dbname=' . self::$db_schema,
+                    self::$db_username,
+                    self::$db_password
                 );
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conn = $conn;                
+
+            } catch (PDOException $e) {
+                die("Error getConnection: <br>" . $e->getMessage());
             }
-
-        } catch (PDOException $e) {
-            die("Error PDO: <br>" . $e);
         }
-
-
-
 
         return self::$conn;
     }
 
+    // _________________________________________
+
+    public static function createTable($sql) {
+        $conn = self::getConnection();        
+
+        try {
+            $conn->exec($sql);
+
+        } catch (PDOException $e) {
+            die("Error createTable: <br>" .  $e->getMessage());
+        }
+    }
+
+    // _________________________________________
+
+    public static function loadENV() {
+
+        self::$db_type = $_ENV['DB_TYPE'];
+        self::$db_host = $_ENV['DB_HOST'];
+        self::$db_schema = $_ENV['DB_SCHEMA'];
+
+        if(gethostbyaddr($_SERVER['REMOTE_ADDR']) === $_ENV['PC_WORK']){
+            self::$db_username = $_ENV['DB_USERNAME_WORK'];
+            self::$db_password = $_ENV['DB_PASSWORD_WORK'];
+        }
+    }
+    // _________________________________________
 }
 
 
---
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/test.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo gethostbyaddr($_SERVER['REMOTE_ADDR']) . '<br>'; // Luke_Desktop.gpc.local
+
+echo $_SERVER['REMOTE_ADDR'];
