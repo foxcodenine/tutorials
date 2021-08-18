d7840ba5c716133ec59bfe45531a7c4785ecdbfb
Christopher Farrugia <foxcode9@gmail.com>
2021-08-18 06:44:59 +0200

    updateds

--- ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/api.sql
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/api.sql
@@ -24,3 +24,22 @@ WHERE Course.id = 3 ORDER BY Student.id;
 INSERT INTO Register (courseId, studentId, RegisterDate) 
 VALUES 
 (2, 2, NOW());
+
+/*
++----+-----------+----------+-----+----------------------+----------+
+| id | firstname | lastname | age | email                | phone    |
++----+-----------+----------+-----+----------------------+----------+
+|  2 | Dorothy   | Cassar   |  31 | dormond@gmail.com    | 99257468 |
+|  3 | Chris     | Farrugia |  37 | chris12aug@yahoo.com | 79345874 |
+|  4 | Tania     | Cardona  |  34 | drvodka@yahoo.com    | 79310545 |
+|  5 | James     | Manduca  |  36 | arch@gove.mt         | 21547852 |
++----+-----------+----------+-----+----------------------+----------+
+
++----+-----------------------------+------------+---------+----------+----------+----------+---------+
+| id | name                        | startDate  | days    | timeFrom | timeTo   | duration | price   |
++----+-----------------------------+------------+---------+----------+----------+----------+---------+
+|  1 | LPIC-1 Linux Administrator  | 2021-10-04 | Mon Wed | 18:00:00 | 21:00:00 |       63 | 1225.00 |
+|  3 | Architecting on AWS Course  | 2022-01-03 | Tue Thu | 18:00:00 | 21:00:00 |       45 | 1000.00 |
+|  4 | MySuccess Website Developer | 2021-11-22 | Mon Wed | 18:00:00 | 21:00:00 |       63 | 1300.00 |
++----+-----------------------------+------------+---------+----------+----------+----------+---------+
+*/
---
+++ ice_malta/mysuccess_web_dev/PHP/practical_exercises/lesson_12/app/Model/Register.php
@@ -0,0 +1,176 @@
+<?php 
+
+namespace app\Model;
+
+use JsonSerializable;
+use PDO;
+use PDOException;
+
+class Register implements JsonSerializable {
+    private static $registerList = Array();
+
+    private $id;
+    private $courseId;
+    private $studentId;
+    private $registerDate;
+
+    // __________________________________
+
+    public function __construct($courseId, $studentId, $registerDate, $id=NULL) {
+        $this->setCourseId($courseId);
+        $this->setStudentId($studentId);
+        $this->setRegisterDate($registerDate);
+        $this->setId($id);
+
+        if (!$id) {
+            $this->addToRegister();
+        }
+    }
+
+    // __________________________________
+
+    public static function checkRegisterExist($courseId, $studentId) {
+        $dbh = DBConnect::getConnection();
+        $sql = 'SELECT * FROM Register 
+                WHERE courseId = :courseId 
+                AND studentId = :studentId';
+
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':courseId', $courseId);
+        $stmt->bindValue(':studentId', $studentId);
+        
+        try {
+            $stmt->execute();            
+            return !empty($stmt->fetchAll(PDO::FETCH_OBJ));
+
+        } catch (PDOException $e) {
+            die('Error checkRegister: <br>' . $e->getMessage());
+        }
+    }  
+
+    // __________________________________
+
+    public function addToRegister() {
+
+        
+        $dbh = DBConnect::getConnection();
+        $sql = 'INSERT INTO Register (courseId, studentId, registerDate) 
+                VALUES (:courseId, :studentId, :registerDate)';
+
+        $stmt = $dbh->prepare($sql);
+
+        $stmt->bindValue(':courseId',     $this->getCourseId());
+        $stmt->bindValue(':studentId',    $this->getStudentId());
+        $stmt->bindValue(':registerDate', $this->getRegisterDate());
+
+        try {
+            $stmt->execute();
+            $this->setId($dbh->lastInsertId);
+            self::updateRegisterList();
+
+        } catch (PDOException $e) {
+            die('Error addToRegister: <br>' . $e->getMessage());
+        } 
+    }
+
+    // __________________________________
+
+    public static function fetchAllRegister() {
+        self::updateRegisterList();
+        return  self::$registerList;
+    }
+
+    // __________________________________
+
+    public static function deleteFromRegister($cID, $sID) {
+        $dbh = DBConnect::getConnection();
+        
+        if ($cID == $sID && strtolower($cID) == 'all') {
+            echo json_encode(['Status'=>'Failure', 'Message'=>'Illegal action']);
+            exit();
+        } else if (strtolower($cID) == 'all') {
+            $sql = 'DELETE FROM Register 
+            WHERE studentId = :studentId';
+        } else if (strtolower($sID) == 'all') {
+            $sql = 'DELETE FROM Register 
+            WHERE courseId = :courseId';
+        } else {
+            $sql = 'DELETE FROM Register 
+            WHERE studentId = :studentId
+            AND courseId = :courseId';
+        }
+
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':studentId', $sID);
+        $stmt->bindValue(':courseId', $cID);
+        try {
+            $stmt->execute();
+            self::updateRegisterList();
+        } catch (PDOException $e) {
+            die('deleteFromRegister: <br>' . $e->getMessage());
+        }  
+    }
+
+    // __________________________________
+
+    public static function updateRegisterList() {
+        $dbh = DBConnect::getConnection();
+        $sql = 'SELECT * FROM Register';
+        $stmt = $dbh->prepare($sql);
+
+        try {
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+            foreach($result as $r) {
+                array_push(
+                    self::$registerList, 
+                    new Register($r->courseId, $r->studentId, $r->registerDate, $r->id)
+                );
+            }
+
+
+        } catch (PDOException $e) {
+            die('Error updateRegisterList: <br>' . $e->getMessage());
+        }
+    }
+
+    // _________________________________________________________________
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getCourseId() {
+        return $this->courseId;
+    }
+    public function setCourseId($courseId) {
+        $this->courseId = $courseId;
+    }
+    public function getStudentId() {
+        return $this->studentId;
+    } 
+    public function setStudentId($studentId) {
+        $this->studentId = $studentId;
+    }
+    public function getRegisterDate() {
+        return $this->registerDate;
+    }
+    public function setRegisterDate($registerDate) {
+        $this->registerDate = $registerDate;
+    }
+
+    // _________________________________________________________________
+
+    public function jsonSerialize() {
+        return array (
+            $this->getId() => [
+                "courseId {$this->getCourseId()}" , 
+                "studentId {$this->getStudentId()}", 
+                Course::arrangeDate($this->getRegisterDate())
+            ]
+        );
+    }
+}
