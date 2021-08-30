f6b82e06e84d323d0a3f72f230acc8949c6871c1
foxcodenine <foxcode9@gmail.com>
2021-08-30 08:57:01 +0200

    testing php bramus router

---
+++ php/php_bramus_router/data/team.php
@@ -0,0 +1,8 @@
+<?php
+
+$team = [
+    1 => ['name'=> 'luka', 'surname' => 'doncic', 'position' => 'frontend developer'],
+    2 => ['name'=> 'jane', 'surname' => 'levy',   'position' => 'backend developer'],
+    3 => ['name'=> 'alexander', 'surname' => 'ludwig', 'position' => 'devops'],
+    4 => ['name'=> 'shanann', 'surname' => 'watts',  'position' => 'full-stack developer']
+];
