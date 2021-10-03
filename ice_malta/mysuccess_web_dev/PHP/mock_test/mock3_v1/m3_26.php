7b1a0140dad90214be2fed2127044bea981be28f
Christopher Farrugia <foxcode9@gmail.com>
2021-10-03 18:30:34 +0200

    study mock test question

---
+++ ice_malta/mysuccess_web_dev/PHP/mock_test/mock3_v1/m3_26.php
@@ -0,0 +1,5 @@
+<?php
+
+$arr = array_combine(array("A", "B", "C"), array(1, 2, 3));
+
+var_dump($arr);
--- ice_malta/mysuccess_web_dev/PHP/mock_test/mock3_v1/m3_67.php
+++ ice_malta/mysuccess_web_dev/PHP/mock_test/mock3_v1/m3_67.php
@@ -2,6 +2,8 @@
 
 $a = array(true, '0'=>false, false=>true);
 
+
 var_export($a);
+
 
 echo '<br><br>', $a['O'];
