96cf30474c6b03c77c6305ec3f73a0d48d852620
Chris Farrugia <foxcode9@gmail.com>
2021-12-22 08:05:55 +0100

    complete realtors section - start media queries

--- udemy_courses/Advanced_CSS_and_Sass_Flexbox_Grid_Animations_and_More!/projects/Nexter/index.php
+++ udemy_courses/Advanced_CSS_and_Sass_Flexbox_Grid_Animations_and_More!/projects/Nexter/index.php
@@ -13,8 +13,13 @@
     <body class="container">
 
         <!-- ------------------------------------------------------- -->
+        
+        <input type="checkbox" class="menu__checkbox" id="menu-checkbox">              
+        <label for="menu-checkbox" class="menu__label"></label>       
+        
 
         <div class="sidebar">
+
             <div class="side">
                 <ul class="side__list">
                     <li class="side__item">
@@ -49,6 +54,7 @@
                     </li>
                 </ul>
             </div>
+
         </div>
 
         <!-- ------------------------------------------------------- -->
@@ -70,7 +76,28 @@
         <!-- ------------------------------------------------------- -->
 
         <div class="realtors">
-            Top 3 realtors
+            <h3 class="heading-3">Top 3 Realtors</h3>
+
+            <div class="realtors__list">
+                <img src="./img/realtor-1.jpeg" alt="Realtor 1" class="realtors__img">
+                <div class="realtors__details">
+                    <h4 class="heading-4 heading-4--light">Erik Feinman</h4>
+                    <p class="realtors__sold">245 houses sold</p>
+                </div>
+  
+                <img src="./img/realtor-2.jpeg" alt="Realtor 2" class="realtors__img">
+                <div class="realtors__details">
+                    <h4 class="heading-4 heading-4--light">Kim Brown</h4>
+                    <p class="realtors__sold">212 houses sold</p>
+                </div>
+
+                <img src="./img/realtor-3.jpeg" alt="Realtor 3" class="realtors__img">
+                <div class="realtors__details">
+                    <h4 class="heading-4 heading-4--light">Toby Ramsey</h4>
+                    <p class="realtors__sold">198 houses sold</p>
+                </div>
+            </div>
+
         </div>
 
         <!-- ------------------------------------------------------- -->
