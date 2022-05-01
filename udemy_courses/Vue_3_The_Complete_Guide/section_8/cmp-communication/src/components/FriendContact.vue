fb56b28e94552d40998cc010aa88b8cea26fcc21
Christopher Farrugia <foxcode9@gmail.com>
2022-05-01 20:08:09 +0200

    update course progress

---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_8/cmp-communication/src/components/FriendContact.vue
@@ -0,0 +1,59 @@
+<template>
+    <li>
+        <h2>{{ name }} <small>{{ friendIsFavorite ? ' Favorite Friend' : '' }}</small></h2>
+
+        <button v-on:click="toggleFavorite" >Toggle Favorite</button>
+
+        <button v-on:click="toggleDetails" >{{ deatailsAreVisible ? 'Hide' : 'Show Details' }}</button>
+
+        <ul v-if="deatailsAreVisible">
+            <li><strong>Phone:</strong> {{ phoneNumber }} </li>
+            <li><strong>Email:</strong> {{ emailAddress }} </li>
+        </ul>
+    </li>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+export default {
+    // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite' ],
+
+    // props: {'name': String, 'phoneNumber': String, 'emailAddress': String, 'isFavorite': String },
+
+    props: {
+        'name':         {type: String, required:true}, 
+        'phoneNumber':  {type: String, required:true}, 
+        'emailAddress': {type: String, required:true}, 
+        'isFavorite':   {
+                            type: Number, 
+                            required:false, 
+                            default: 0,
+                            validator : function(val) {
+                                return val === 1 || val === 0;
+                            }
+                        } 
+    },
+    // https://v3.vuejs.org/guide/component-props.html
+
+    data() {
+        return {
+            deatailsAreVisible: false,
+            friendIsFavorite: this.isFavorite
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.deatailsAreVisible = !this.deatailsAreVisible;
+        },
+        toggleFavorite() {
+            this.friendIsFavorite = Number(!this.friendIsFavorite);
+        }
+    }
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="scss">
+</style>
