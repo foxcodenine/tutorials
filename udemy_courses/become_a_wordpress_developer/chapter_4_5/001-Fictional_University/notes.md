b1174a0894245e616f6933040650ed4157ed2264
Chris Farrugia <foxcode9@gmail.com>
2022-05-30 19:47:44 +0200

    wordpress - update course progress - complete chapter 5

--- udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/notes.md
+++ udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/notes.md
@@ -1,3 +1,9 @@
+### Docs
+
+https://developer.wordpress.org/
+
+https://codex.wordpress.org/Main_Page
+
 ### Wordpress Function:
 
 ```php
@@ -12,18 +18,35 @@
     the_post();
 
     the_title();
+    get_the_title('#enter_an_id');
 
     the_content();
 
 
 
     the_permalink();
+    get_permalink('#enter_an_id')
 
     get_header();
     get_footer();
 
     wp_head();
     wp_footer();
+
+    <html <?php language_attributes() ?> 
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <body <?php body_class(); ?> >
+
+
+    site_url('/privacy-policy')
+    get_theme_file_uri('/build/index.js')
+    get_stylesheet_uri();
+    
+    the_ID();
+    get_the_ID();
+
+    wp_get_post_parent_id( '#enter_an_id' );
 
 ```
 
@@ -46,4 +69,36 @@
      * 'wp_enqueue_scripts'
     */
 
+```
+
+
+```php
+    function myFunction() {
+        register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme', 'myFunction');
+
+    /** wp function used above
+     * 
+     * add_action(), add_theme_support(), register_nav_menu()
+    */
+
+    /** wp Hooks used above
+     * 
+     * 'after_setup_theme'
+    */
+
+    /** feature
+     * 
+     * 'title-tag'
+    */
+
+```
+
+```php
+    wp_list_pages( [ 'title_li' => NULL, 'child_of' => $theId, 'sort_column' => 'menu_order' ] );
+
+    wp_nav_menu(['theme_location' => 'headerMenuLocation']);
 ```
--- udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/footer.php
+++ udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/footer.php
@@ -4,7 +4,7 @@
         <div class="group">
           <div class="site-footer__col-one">
             <h1 class="school-logo-text school-logo-text--alt-color">
-              <a href="#"><strong>Fictional</strong> University</a>
+              <a href="<?= site_url()?>"><strong>Fictional</strong> University</a>
             </h1>
             <p><a class="site-footer__link" href="#">555.555.5555</a></p>
           </div>
@@ -13,12 +13,15 @@
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
               <nav class="nav-list">
-                <ul>
-                  <li><a href="#">About Us</a></li>
+                  <?php wp_nav_menu(['theme_location' => 'footerLocationOne'])?>
+                  <!-- 
+                  <ul>
+                  <li><a href="<?= site_url('/about-us')?>">About Us</a></li>
                   <li><a href="#">Programs</a></li>
                   <li><a href="#">Events</a></li>
                   <li><a href="#">Campuses</a></li>
-                </ul>
+                  </ul> 
+                  -->
               </nav>
             </div>
 
@@ -26,9 +29,12 @@
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
                 <ul>
+                  <!-- 
                   <li><a href="#">Legal</a></li>
-                  <li><a href="#">Privacy</a></li>
-                  <li><a href="#">Careers</a></li>
+                  <li><a href="<?= site_url('/privacy-policy')?>">Privacy</a></li>
+                  <li><a href="#">Careers</a></li> 
+                  -->
+                  <?php wp_nav_menu(['theme_location' => 'footerLocationTwo'])?>
                 </ul>
               </nav>
             </div>
