46ef12d8287bc93a25b8199e8888574147a3af05
foxcodenine <foxcode9@gmail.com>
2022-05-30 19:47:44 +0200

    wordpress - update course progress - complete chapter 5

--- udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/header.php
+++ udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/header.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes() ?> >
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <body>
+    <body <?php body_class(); ?> >
         <!-- header start --------------------------------------------->
         <header class="site-header">`
             <div class="container">
                 <h1 class="school-logo-text float-left">
-                <a href="#"><strong>Fictional</strong> University</a>
+                <a href="<?= site_url()?>"><strong>Fictional</strong> University</a>
                 </h1>
                 <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
                 <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
                 <div class="site-header__menu group">
                 <nav class="main-navigation">
+                    <!-- <?php wp_nav_menu(['theme_location' => 'headerMenuLocation']) ?> -->
+
+                    <?php function isActive($pageName, $pageId) {
+                        return  is_page($pageName) || wp_get_post_parent_id(0) == 
+                                            $pageId ? 'current-menu-item' : '';
+                    }?>
+                    
                     <ul>
-                    <li><a href="#">About Us</a></li>
+                    <li class="<?= isActive('about-us', 16) ?>" ><a href="<?= site_url('/about-us')?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Campuses</a></li>
                     <li><a href="#">Blog</a></li>
-                    </ul>
+                    </ul> 
+                   
                 </nav>
                 <div class="site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
--- udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/page.php
+++ udemy_courses/become_a_wordpress_developer/chapter_4_5/001-Fictional_University/wp-content/themes/fictional-university-theme/page.php
@@ -2,9 +2,61 @@
 
     <?php the_post();?>
 
-    <h1>This is a Page not a post</h1>
-    <h2><?= the_title();?> </h2>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DONT FORGOET TO REPLCE ME LATER</p>
+        </div>
+      </div>
+    </div>
 
-    <?= the_content();?>
+    <div class="container container--narrow page-section">
+
+		<?php $theParentId = wp_get_post_parent_id( get_the_ID()) ?>
+		<?php if ($theParentId !== 0):?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>        
+					<a class="metabox__blog-home-link" href="<?= get_permalink($theParentId) ?>">
+						<i class="fa fa-home" aria-hidden="true"></i><?= get_the_title($theParentId)?>
+					</a>					
+					<span class="metabox__main"><?= the_title(); ?></span>
+        </p>
+      </div>
+		<?php endif ?>
+
+      
+		<?php $isAParent = (bool) get_pages(['child_of' => get_the_id()]) ?>
+		<?php if( $theParentId || $isAParent): ?>
+			<div class="page-links">
+					<?php 
+						// if it is a parent return its id, and if it is a child return its parents id
+						$theId = $theParentId ?: get_the_ID();
+					?>
+        <h2 class="page-links__title"><a href="<?= get_permalink($theId) ?>"><?= get_the_title($theId); ?></a></h2>
+
+        <ul class="min-list">
+
+						<?php 
+						wp_list_pages( [
+							'title_li' => NULL, 
+							'child_of' => $theId, 
+							'sort_column' => 'menu_order'
+						] );						
+						?>
+
+        </ul>
+
+      </div> 
+		<?php endif ?>
+			
+
+      <div class="generic-content">
+				<?php the_content(); ?>
+    	</div>
+
+    </div>
+
 
 <?php   endwhile; get_footer(); ?>
