c3d0ecc4a2f68f8292c6a1d780773acc868a6259
Christopher Farrugia <foxcode9@gmail.com>
2022-06-01 21:51:11 +0200

    wordpress - started chapter 7

---
+++ udemy_courses/become_a_wordpress_developer/chapter_7_8_9/001-Fictional_University/wp-content/themes/fictional-university-theme/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?> >
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <body <?php body_class(); ?> >
+        <!-- header start --------------------------------------------->
+        <header class="site-header">`
+            <div class="container">
+                <h1 class="school-logo-text float-left">
+                <a href="<?= site_url()?>"><strong>Fictional</strong> University</a>
+                </h1>
+                <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+                <div class="site-header__menu group">
+                <nav class="main-navigation">
+                    <!-- <?php wp_nav_menu(['theme_location' => 'headerMenuLocation']) ?> -->
+
+                    <?php function isActive($pageName=false, $pageId=false, $postType=false, ) {
+                        return  is_page($pageName) || 
+                                wp_get_post_parent_id(0) === $pageId ||
+                                get_post_type() === $postType  ? 
+                                'current-menu-item' : '';
+                    }?>
+                    
+                    <ul>
+                    <li class="<?= isActive('about-us', 16) ?>" ><a href="<?= site_url('/about-us')?>">About Us</a></li>
+                    <li><a href="#">Programs</a></li>
+                    <li class="<?= isActive('event', false, 'event') ?>"><a href="<?= get_post_type_archive_link('event'); ?>">Events</a></li>
+                    <li><a href="#">Campuses</a></li>
+                    <li class="<?= isActive('blog', false, 'post') ?>"><a href="<?= site_url('/blog'); ?>">Blog</a></li>
+                    </ul> 
+                   
+                </nav>
+                <div class="site-header__util">
+                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                </div>
+                </div>
+            </div>
+        </header>
+        
+        <!-- header end ----------------------------------------------->
---
+++ udemy_courses/become_a_wordpress_developer/chapter_7_8_9/001-Fictional_University/wp-content/themes/fictional-university-theme/page.php
@@ -0,0 +1,67 @@
+<?php get_header();   while(have_posts()): ?>
+
+    <?php the_post();?>
+
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DONT FORGOET TO REPLCE ME LATER</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+
+		<?php $theParentId = wp_get_post_parent_id( get_the_ID()) ?>
+
+		<!-- Metabox start -------------------------------------------->
+		<?php if ($theParentId !== 0):?>
+		<div class="metabox metabox--position-up metabox--with-home-link">
+			<p>        
+				<a class="metabox__blog-home-link" href="<?= get_permalink($theParentId) ?>">
+					<i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($theParentId)?>
+				</a>					
+				<span class="metabox__main"><?= the_title(); ?></span>
+			</p>
+		</div>
+		<?php endif ?>
+		<!-- Metabox End ---------------------------------------------->
+
+		<!-- Side Menu start ------------------------------------------>
+		<?php $isAParent = (bool) get_pages(['child_of' => get_the_id()]) ?>
+		<?php if( $theParentId || $isAParent): ?>
+		<div class="page-links">
+			<?php 
+				// if it is a parent return its id, and if it is a child return its parents id
+				$theId = $theParentId ?: get_the_ID();
+			?>
+			<h2 class="page-links__title"><a href="<?= get_permalink($theId) ?>"><?= get_the_title($theId); ?></a></h2>
+
+			<ul class="min-list">
+
+							<?php 
+							wp_list_pages( [
+								'title_li' => NULL, 
+								'child_of' => $theId, 
+								'sort_column' => 'menu_order'
+							] );						
+							?>
+
+			</ul>
+
+      	</div> 
+		<?php endif ?>
+		<!-- Side Menu End -------------------------------------------->
+			
+		<!-- Content Start -------------------------------------------->
+      	<div class="generic-content">
+				<?php the_content(); ?>
+    	</div>
+		<!-- Content End ---------------------------------------------->
+
+    </div>
+
+
+<?php   endwhile; get_footer(); ?>
