0cb40832877662a471dfafb13b78f2c516b9e532
foxcodenine <foxcode9@gmail.com>
2022-06-03 13:56:20 +0200

    wordpress - update course progress - complete chapter 9

--- udemy_courses/become_a_wordpress_developer/chapter_7_8_9/001-Fictional_University/wp-content/themes/fictional-university-theme/front-page.php
+++ udemy_courses/become_a_wordpress_developer/chapter_7_8_9/001-Fictional_University/wp-content/themes/fictional-university-theme/front-page.php
@@ -30,35 +30,34 @@
             'meta_query' => [
                 ['key' => 'event_date', 'compare' => '>=', 'value' => $today, 'type' => 'numeric']
             ]
-
         ]);
     ?>
 
-<div class="full-width-split__one">
-    <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-        
-        <?php while ($homepageEvents->have_posts()): $homepageEvents->the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?= get_the_permalink(); ?>">
-                    <?php $eventDate = new DateTime(get_field('event_date'))?>
-                    <span class="event-summary__month"><?= $eventDate->format('M')?></span>
-                    <span class="event-summary__day"><?= $eventDate->format('d')?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny">
-                        <a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a>
-                    </h5>
-                    <p>
-                        <?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> 
-                        <a href="<?= get_the_permalink(); ?>" class="nu gray">Learn more</a>
-                    </p>
+    <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+            <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+            
+            <?php while ($homepageEvents->have_posts()): $homepageEvents->the_post(); ?>
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="<?= get_the_permalink(); ?>">
+                        <?php $eventDate = new DateTime(get_field('event_date'))?>
+                        <span class="event-summary__month"><?= $eventDate->format('M')?></span>
+                        <span class="event-summary__day"><?= $eventDate->format('d')?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a>
+                        </h5>
+                        <p>
+                            <?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> 
+                            <a href="<?= get_the_permalink(); ?>" class="nu gray">Learn more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        <?php endwhile ?>
-        
-        <p class="t-center no-margin"><a href="<?= get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
-    </div>
+            <?php endwhile ?>
+            
+            <p class="t-center no-margin"><a href="<?= get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
+        </div>
     </div>
     <!-- From Upcoming Evets end -------------------------------------->
     
