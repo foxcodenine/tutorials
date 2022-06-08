4b0e230142da57a05f4665de64442210b7a73106
Christopher Farrugia <foxcode9@gmail.com>
2022-06-08 18:17:44 +0200

    complete chapters 12 and 13

---
+++ udemy_courses/become_a_wordpress_developer/chapter_13_14_15/001-Fictional_University/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e2b0c4736303de4ea914');
---
+++ udemy_courses/become_a_wordpress_developer/chapter_13_14_15/001-Fictional_University/wp-content/themes/fictional-university-theme/template-parts/content-event.php
@@ -0,0 +1,16 @@
+<div class="event-summary">
+    <a class="event-summary__date t-center" href="<?= get_the_permalink(); ?>">
+        <?php $eventDate = new DateTime(get_field('event_date'))?>
+        <span class="event-summary__month"><?= $eventDate->format('M')?></span>
+        <span class="event-summary__day"><?= $eventDate->format('d')?></span>
+    </a>
+    <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny">
+            <a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a>
+        </h5>
+        <p>
+            <?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> 
+            <a href="<?= get_the_permalink(); ?>" class="nu gray">Learn more</a>
+        </p>
+    </div>
+</div>
