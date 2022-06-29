2af5b6c55c480ad29b0e5c2fb121cfc9aeee30ce
Christopher Farrugia <foxcode9@gmail.com>
2022-06-29 21:17:00 +0200

    updateing course progress

--- udemy_courses/become_a_wordpress_developer/chapter_19_20_21/001-Fictional_University/wp-content/themes/fictional-university-theme/page-my-notes.php
+++ udemy_courses/become_a_wordpress_developer/chapter_19_20_21/001-Fictional_University/wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -26,11 +26,12 @@
 		<ul class="min-list">
 		<?php while($userNotes->have_posts()): $userNotes->the_post() ?>
 			
-			<li>
+			<li data-id="<?php the_ID(); ?>">
 				<input class="note-title-field" value="<?= esc_attr(get_the_title()) ?>">
 				<span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
 				<span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
 				<textarea class="note-body-field"><?= esc_attr(wp_strip_all_tags(get_the_content())) ?></textarea>
+				<span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
 			</li>
 
 		<?php endwhile; wp_reset_postdata();?>
