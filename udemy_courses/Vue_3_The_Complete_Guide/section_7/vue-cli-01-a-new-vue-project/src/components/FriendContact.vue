04d07b271d16b527d15509c7efe839933fd6d00c
Chris Farrugia <foxcode9@gmail.com>
2022-08-02 16:15:13 +0200

    Vue update course progress complete chapter 7

---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button v-on:click="toggleDetails" >{{ detailsAreVisable ? 'Hide Details' : 'Show Details' }}</button>
+        <ul v-if="detailsAreVisable">
+            <li><strong>Phone:</strong> {{ friend.phone }} </li>
+            <li><strong>Email:</strong> {{ friend.email }} </li>
+        </ul>
+    </li>
+</template>
+
+<!-- -------------------------------------------------------------- -->
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisable:  false,
+        }
+	},
+    props: { friend: Object},
+    watch: {        
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisable = !this.detailsAreVisable;
+        }
+	},
+    computed: {
+
+    }
+}
+</script>
+
+<!-- -------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+
+</style>
