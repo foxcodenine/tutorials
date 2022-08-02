2de1f753613f695b7d93d6e1f9055ecc00e8110a
Chris Farrugia <foxcode9@gmail.com>
2022-08-02 16:17:18 +0200

    Laravel update course progress complete chapter 33

---
+++ udemy_courses/Master_Laravel_8_for_Beginners_and_Intermediate/chapter_33_polymorphoc_eloquent_relations/app/Http/Controllers/PostTagController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use App\Models\Tag;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class PostTagController extends Controller
+{
+    public function index($tag) 
+    {
+        $tag = Tag::findOrFail($tag);
+        // $tag = Tag::where('id', $tag)->with(['blogPosts', 'blogPosts.user'])->first();
+       
+        
+        // ---- Side bar Data ------------------------------------------
+
+        // ~~> NOTICE: This is not required anymore, since we are useing a view composer instead
+
+        // $mostCommented = Cache::tags(['blog-post'])->remember('most-commented-blog-posts', Carbon::now()->addSecond(60), function() {
+        //     return BlogPost::mostCommented()->take(5)->get();
+        // }); 
+
+        // $mostActive = Cache::tags(['blog-post'])->remember('most-active-users', Carbon::now()->addSecond(60), function() {
+        //     return User::withMostBlogPosts()->take(5)->get();
+        // });
+
+        // $mostActiveLastMonth = Cache::tags(['blog-post'])->remember('most-active-users-last-month', Carbon::now()->addSecond(60), function() {
+        //     return User::mostActiveLastMonth()->take(5)->get();
+        // });
+
+        // -------------------------------------------------------------
+
+        return view('posts.index', [
+            'posts' => $tag->blogPosts()->withAllRelations()->with('comments.user')->get(),
+            // 'mostCommented' => $mostCommented,
+            // 'mostActive' => $mostActive,
+            // 'mostActiveLastMonth' => $mostActiveLastMonth,
+        ]);
+    }
+}
---
+++ udemy_courses/Master_Laravel_8_for_Beginners_and_Intermediate/chapter_33_polymorphoc_eloquent_relations/app/Models/Image.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+
+class Image extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+protected $fillable = ['path', /*'blog_post_id'*/];
+
+    static protected $default = '/images/fox.jpg';
+    
+    // _________________________________________________________________
+
+
+    public static function default() {
+        return URL::asset(static::$default);
+    }
+    
+
+    // ----- Relationship Methods --------------------------------------
+
+    // --- UPDATED:
+    // public function blogPost() {
+    //     return $this->belongsTo(BlogPost::class);        
+    // }
+    // --- TO:
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
+
+    // _________________________________________________________________
+
+    public function url () {
+        $path = $this->path;
+
+        if (in_array(substr($path, 0, 7), ['http://', 'https:/'], true) ) {
+            return $path;
+        }
+
+        return Storage::url($path);       
+    }
+}
