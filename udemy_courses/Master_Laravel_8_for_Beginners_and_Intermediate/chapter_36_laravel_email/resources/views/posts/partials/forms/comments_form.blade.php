1790d309b33974f1b07551290afd234b9b29701c
foxcodenine <foxcode9@gmail.com>
2022-08-08 22:06:51 +0200

    Laravel course -complete chapter 36

---
+++ udemy_courses/Master_Laravel_8_for_Beginners_and_Intermediate/chapter_36_laravel_email/resources/views/posts/partials/forms/comments_form.blade.php
@@ -0,0 +1,18 @@
+{{-- Using comment-form.blade.php instead of this  --}}
+{{-- <div class="my-4">
+
+@auth
+    <form action="{{ route('posts.comments.store', ['post' => $post->id]) }}" method="POST">
+        @csrf
+
+        <div class="from-group mb-3">
+            <textarea name="content" class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary w-100">Add comment</button>
+    </form>
+    <x-errors/>
+@else
+    <a href="{{ route('login') }}">Sign-in</a> to post comments!
+@endauth
+</div> --}}
