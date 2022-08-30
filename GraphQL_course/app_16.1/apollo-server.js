1db86ff40a5f59fad44012d57e84ec35a48cbbc0
foxcodenine <foxcode9@gmail.com>
2022-08-30 21:01:22 +0200

    graphql - update progress

---
+++ GraphQL_course/app_16.1/apollo-server.js
@@ -0,0 +1,117 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const {
+    ApolloServerPluginLandingPageLocalDefault
+  } = require('apollo-server-core');
+
+// ----- Database ------------------------------------------------------
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+
+// ----- GraphQL schema ------------------------------------------------
+
+// * gql`...` is like buildSchema()
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+// This "Book" type defines the queryable fields for every book in our
+// data source.
+
+
+// The "Query" type is special: it lists all of the available queries
+// that clients can execute, along with the return type for each. In
+// this case, the "books" query returns an array of zero or more Books
+// (defined above).
+
+// ----- Resolver ------------------------------------------------------
+
+const resolvers = {
+    Query: {
+
+      books: () => {
+        return books
+      },
+
+    },
+  };
+
+
+// ----- Create an instance of ApolloServer ----------------------------
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
+});
+
+// * typeDefs & resolvers
+// The ApolloServer constructor requires two parameters: 
+  // Your schema definition 'typeDefs'
+  // And your set of resolvers.
+
+// * CSRF
+// We also want to enable CSRF prevention.
+
+// * cache
+// And pass cache: "bounded" or configure their cache in a manner that
+// isn't unbounded (which is current default behavior). This protects
+// your server from attacks that exhaust available memory, causing a DOS
+
+// * ApolloServerPluginLandingPageProductionDefault
+// What's up with this embed: true option? These are our recommended
+// settings for using AS; they aren't the defaults in AS3 for
+// backwards-compatibility reasons but will be the defaults in AS4. For
+// production environments, use
+// ApolloServerPluginLandingPageProductionDefault instead.
+
+
+
+// ----- The `listen` method launches a web server. --------------------
+
+server.listen(8080).then( ({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
+// ---------------------------------------------------------------------
+
+/*
+
+query book_list  {
+    books {
+      title
+      author
+    }
+}
+
+*/
--- GraphQL_course/notes.md
+++ GraphQL_course/notes.md
@@ -13,6 +13,8 @@
 
 #### Installing Express and express-graphql 
 
+https://graphql.org/code/#javascript
+
     $ npm install express express-graphql --save
 
     $ npm install express express-graphql graphql --save
@@ -34,7 +36,29 @@
     $ nodemon ./server.js localhost 8080
 
 
-
-
-
 <!-- npm init -y; npm install express express-graphql graphql cors --save; npm install --save-dev nodemon -->
+
+#### Install the Apollo - Server - Express
+
+    $ npm install apollo-server-express apollo-server-core express graphql --save
+
+
+#### Install the Apollo - Server - Express
+
+    $ npm install apollo-server-express apollo-server-core express graphql --save
+
+
+#### Install the Apollo - Server
+
+https://www.apollographql.com/docs/apollo-server/getting-started
+
+    $ npm install apollo-server graphql --save
+    
+
+#### Killing a process on a particular port 
+
+    $ lsof -i:8080
+    > node    21155 foxcodenine   23u  IPv6  95035      0t0  TCP ip6-localhost...
+
+    $ kill 21155
+    
