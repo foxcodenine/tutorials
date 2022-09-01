8c59601fac5cfeb26320469c49d2c6b921f551bc
Christopher Farrugia <foxcode9@gmail.com>
2022-09-01 23:05:46 +0200

    Vue3 updateing course progress

---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_10/prj-cmp-01/src/components/learning-resource/StoredResources.vue
@@ -0,0 +1,57 @@
+<template>
+    <ul>
+        <learning-resourse 
+            v-for="res in resouces" 
+            :key="res.id"
+            :id="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        >
+        </learning-resourse>        
+    </ul>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+import LearningResourse from './LearningResourse.vue'
+export default { 
+
+    components: {
+        LearningResourse
+    },
+
+    inject: ['resouces'],
+
+    data() {
+        return {
+
+        }
+    },
+
+    watch: {
+
+    },
+
+    computed: {
+
+    },
+    
+    methods: {
+        
+    },
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_9/cmp-adv-07/src/components/ManageGoals.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal">
+    <button v-on:click="setGoal">Set Goal</button>
+
+    <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+        <h2>Input must not be empty</h2>
+        <p>Please enter at least a few characters...</p>
+        <button v-on:click="confirmError">Okey</button>
+    </error-alert>
+    </teleport>
+
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+    components: { ErrorAlert },
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
+    methods: {
+        setGoal() {
+            const enteredValur = this.$refs.goal.value;
+
+            if (enteredValur.trim() === '') {                
+                this.inputIsInvalid = true;                
+            }
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
+        }
+    }
+}
+</script>
