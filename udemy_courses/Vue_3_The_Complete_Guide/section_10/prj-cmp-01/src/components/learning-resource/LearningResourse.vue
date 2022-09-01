dbac4d993fd4f8725cc2655e1e849cd90781882e
Chris Farrugia <foxcode9@gmail.com>
2022-09-01 23:05:46 +0200

    Vue3 updateing course progress

---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_10/prj-cmp-01/src/components/learning-resource/LearningResourse.vue
@@ -0,0 +1,82 @@
+<template>
+    <li>
+      <base-card>
+            <header>
+                <h3>{{title}}</h3>
+                <base-button mode="flat" v-on:click="removeResources(id)">Delete</base-button>
+            </header>
+            <p>{{description}}</p>
+            <nav>
+                <a :href="link">View Resourse</a>
+            </nav>
+      </base-card>
+    </li>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+export default { 
+
+    props: {
+        id: [Number, String],
+        title: String,
+        description: String,
+        link: String,
+    },
+
+    inject: ['removeResources'],
+
+    data() {
+        return {
+
+        }
+    },
+
+    watch: {
+
+    },
+
+    computed: {
+
+    },
+    
+    methods: {
+        
+    }
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+    li {
+        margin: auto;
+        max-width: 40rem;
+      }
+      
+      header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      
+      h3 {
+        font-size: 1.25rem;
+        margin: 0.5rem 0;
+      }
+      
+      p {
+        margin: 0.5rem 0;
+      }
+      
+      a {
+        text-decoration: none;
+        color: #ce5c00;
+      }
+      
+      a:hover,
+      a:active {
+        color: #c89300;
+      }
+</style>
---
+++ udemy_courses/Vue_3_The_Complete_Guide/section_9/cmp-adv-01/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals>
+      <template v-slot:default="slotProps" >
+        <h2>{{ slotProps.itemProp }}</h2>
+        <div class="capitalize">{{ slotProps.anotherProp }}</div>
+      </template>
+    </course-goals>
+  </div>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+export default {
+  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+.capitalize {
+  text-transform: capitalize;
+}
+</style>
