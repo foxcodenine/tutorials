6e9bbc6818a600543a506296de0df6eef191f52f
Christopher Farrugia <foxcode9@gmail.com>
2022-10-29 13:46:48 +0200

    LaravelVue - updating course progess

---
+++ udemy_courses/Master_Laravel_With_Vue/013_laravelbnb/resources/js/components/bookables/BookableListItem.vue
@@ -0,0 +1,26 @@
+<template >
+    <div class="card mb-2">
+        <div class="card-body">
+            <h2 class="card-title">{{ itemTitle }}</h2>
+            <p class="card-text">{{ itemContent }}</p>
+        </div>
+    </div>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+export default {
+    props: {
+        'itemTitle': String, 
+        'itemContent': String, 
+        'price': Number
+    }
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+    
+</style>
