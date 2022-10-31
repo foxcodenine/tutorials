2f44b03ebaa7f631a3a45cecf4628dbe43b3340b
Chris Farrugia <foxcode9@gmail.com>
2022-10-31 22:58:30 +0100

    updating course progress

--- udemy_courses/Master_Laravel_With_Vue/014_laravelbnb/routes/web.php
+++ udemy_courses/Master_Laravel_With_Vue/014_laravelbnb/routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any?}', function () {
     return view('welcome');
-})->where('any', '^(?!013/api\/)[\/\w\.-]*');
+})->where('any', '^(?!014/api\/)[\/\w\.-]*');
