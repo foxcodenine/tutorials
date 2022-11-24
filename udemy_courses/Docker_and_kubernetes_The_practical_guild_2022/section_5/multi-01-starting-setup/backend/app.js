635a2667427b55709156c7a589e4fe713fff2d08
Christopher Farrugia <foxcode9@gmail.com>
2022-11-24 21:06:55 +0100

    update course progess

--- udemy_courses/Docker_and_kubernetes_The_practical_guild_2022/section_5/multi-01-starting-setup/backend/app.js
+++ udemy_courses/Docker_and_kubernetes_The_practical_guild_2022/section_5/multi-01-starting-setup/backend/app.js
@@ -85,7 +85,8 @@ app.delete('/goals/:id', async (req, res) => {
 
 mongoose.connect(
   // 'mongodb://172.17.0.2:27017/course-goals',
-  'mongodb://mongodb:27017/course-goals',
+  // 'mongodb://mongodb:27017/course-goals',
+  'mongodb://ubuntu:secret@mongodb:27017/course-goals?authSource=admin',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
