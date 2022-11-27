e6bc7cbbad18feaf735b206b0978b423ade7540c
foxcodenine <foxcode9@gmail.com>
2022-11-27 22:14:00 +0100

    update course progress

---
+++ udemy_courses/Master_Laravel_With_Vue/014_laravelbnb/resources/js/components/shared/StarRating.vue
@@ -0,0 +1,36 @@
+<template >
+    <div d-flex >
+        <font-awesome-icon icon="fa-solid fa-star" v-for="star in fullStars" :key="'full' + star"/>
+        <font-awesome-icon icon="fa-solid fa-star-half-alt"  v-for="star in halfStars" :key="'half' + star"/>
+        <font-awesome-icon icon="fa-regular fa-star"  v-for="star in emptyStars" :key="'empty' + star"/>
+    </div>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+export default {
+    props: {
+        rating: Number
+    },
+    computed: {
+        fullStars() {
+            return Math.round(this.rating);
+        },
+        halfStars() {
+            const fraction = Math.round((this.rating - Math.floor(this.rating)) * 100);
+            console.log(fraction > 0 && fraction < 50)
+            return fraction > 0 && fraction < 50 ? 1 : 0;
+        },
+        emptyStars() {
+            return 5 - Math.ceil(this.rating);;
+        }
+    }
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="scss" scoped>
+    
+</style>
