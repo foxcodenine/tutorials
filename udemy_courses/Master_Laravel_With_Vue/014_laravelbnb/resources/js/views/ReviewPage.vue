3b7d2deddfd5375468976e4b92500feed6ab32a4
Chris Farrugia <foxcode9@gmail.com>
2022-12-05 21:35:49 +0100

    update course progress

--- udemy_courses/Master_Laravel_With_Vue/014_laravelbnb/resources/js/views/ReviewPage.vue
+++ udemy_courses/Master_Laravel_With_Vue/014_laravelbnb/resources/js/views/ReviewPage.vue
@@ -1,28 +1,44 @@
 <template >
-    <div>
-        <div v-if="loading">Loading...</div>
-        <div v-else>
-            <div v-if="alreadyReviewed">
-                <h3>You've have aleardy left a review for this booking!</h3>
+    <div class="row">
+        <div :class="{'col-md-4': loading || !alreadyReviewed, 'd-none':  !loading && alreadyReviewed}">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="loading">Loading...</div>
+                    <div v-else>
+                        <p>
+                            Stayed at 
+                            <router-link :to="{name: 'bookable', params: {id: booking.bookable.bookable_id} }">{{booking.bookable.title}}</router-link>
+                        </p>
+                        <p>From {{ booking.from }} to {{ booking.to }}</p>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div :class="{'col-md-8': loading || !alreadyReviewed, 'col-md-12':  !loading && alreadyReviewed}" >
+            <div v-if="loading">Loading...</div>
             <div v-else>
-                <div class="from-group">
-                    <label  class="text-muted">
-                        Select the star rating (1 is worst - 5 is best)
-                        <star-rating  class="fa-3x"                 
-                        v-model="review.rating"
-                        ></star-rating>
-                    </label>
+                <div v-if="alreadyReviewed">
+                    <h3>You've have aleardy left a review for this booking!</h3>
                 </div>
-                <div class="from-group">
-                    <label for="content" class="text-muted">
-                        Describe your expirience with us:
-                    </label>
-                    <textarea name="content" cols="30" rows="10"
-                    class="form-control" v-model="review.content">
-                    </textarea>
+                <div v-else>
+                    <div class="from-group">
+                        <label  class="text-muted">
+                            Select the star rating (1 is worst - 5 is best)
+                            <star-rating  class="fa-3x"                 
+                            v-model="review.rating"
+                            ></star-rating>
+                        </label>
+                    </div>
+                    <div class="from-group">
+                        <label for="content" class="text-muted">
+                            Describe your expirience with us:
+                        </label>
+                        <textarea name="content" cols="30" rows="10"
+                        class="form-control" v-model="review.content">
+                        </textarea>
+                    </div>
+                    <button class="btn btn-lg btn-primary mt-4 w-100">Submit</button>
                 </div>
-                <button class="btn btn-lg btn-primary mt-4 w-100">Submit</button>
             </div>
         </div>
     </div>
@@ -49,8 +65,14 @@ export default {
         }
     },
     computed: {
-        alreadyReviewed() {
+        alreadyReviewed() {            
+           return this.hasReview || !this.hasBooking;
+        },
+        hasReview() {
             return this.existingReview !== null;
+        },
+        hasBooking() {
+            return this.booking !== null;
         }
     },
     async created() {
@@ -66,26 +88,29 @@ export default {
             
             if (error.response && error.response.status && error.response.status === 404 ) {
                 
+                // - 2. Fetch a booking by a review key
                 try {
                     let response = await axios.get(`/api/booking-by-review/${this.$route.params.id}`);
                     this.booking = response.data.data;
+                    this.loading = false;
 
                 } catch (error) {
                     console.log(error)
                 }                
                 
             } else {
+                
                 console.log(error);
             }
 
         } finally { 
+            
             console.log(this.existingReview)           
-            console.log(this.booking)           
-            this.loading = false;
+            console.log(this.booking) 
+            console.log(this.loading, !this.alreadyReviewed)          
         }
         
 
-        // - 2. Fetch a booking by a review key
         // - 3. Store the review
     },
 }
