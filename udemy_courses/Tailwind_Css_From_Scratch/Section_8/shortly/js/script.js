dc918bb79ffc3db5a40154d248855696a1c80170
foxcodenine <foxcode9@gmail.com>
2023-03-04 11:49:15 +0100

    Tailwind - update course progress

---
+++ udemy_courses/Tailwind_Css_From_Scratch/Section_8/shortly/js/script.js
@@ -0,0 +1,63 @@
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+
+linkForm.addEventListener('submit', formSubmit)
+
+
+
+// --- Submit form, Validation ----------------
+
+function formSubmit(e) {
+	e.preventDefault()
+
+	if (input.value === '') {
+		errMsg.innerHTML = 'Please enter something'
+		input.classList.add('border-red')
+
+	} else if (!validURL(input.value)) {
+		errMsg.innerHTML = 'Please enter a valid URL'
+		input.classList.add('border-red')
+
+	} else {
+		errMsg.innerHTML = ''
+		input.classList.remove('border-red')
+		alert('Success')
+	}
+}
+
+// --- Validate a URL ------------------------
+
+function validURL(str) {
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' + // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+		'((\\d{1,3}\\.){3}\\d{1,3}))' +
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+		'(\\?[;&a-z\\d%_.~+=-]*)?' +
+		'(\\#[-a-z\\d_]*)?$',
+		'i'
+	)
+	return !!pattern.test(str)
+}
+
+
+// ---------------------------------------------------------------------
+
+
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+/*
+
+btn.addEventListener('click', navToggle)
+
+// Toggle Mobile Menu
+function navToggle() {
+	btn.classList.toggle('open')
+	menu.classList.toggle('flex')
+	menu.classList.toggle('hidden')
+}
+
+*/
+
