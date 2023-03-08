61d103f48f32035fc2b625d03bc7aecb369540f9
Chris Farrugia <foxcode9@gmail.com>
2023-03-08 19:01:43 +0100

    update course progress

--- udemy_courses/Tailwind_Css_From_Scratch/Section_8/shortly/js/script.js
+++ udemy_courses/Tailwind_Css_From_Scratch/Section_8/shortly/js/script.js
@@ -43,21 +43,21 @@ function validURL(str) {
 }
 
 
-// ---------------------------------------------------------------------
+// --- Toggle Mobile Menu ----------------------------------------------
 
 
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
-/*
+
 
 btn.addEventListener('click', navToggle)
 
-// Toggle Mobile Menu
+
 function navToggle() {
 	btn.classList.toggle('open')
 	menu.classList.toggle('flex')
 	menu.classList.toggle('hidden')
 }
 
-*/
+
 
