071b05faabb8d9131d6e2849859504f931aeb7ed
Christopher Farrugia <foxcode9@gmail.com>
2023-04-15 11:33:00 +0200

    Node js course - update progress

---
+++ udemy_courses/NodeJs-The_complet_guide/section_2_javascript_a_quick_refresher/notes.md
@@ -0,0 +1,9 @@
+
+
+### Reference vs Primitive Values
+
+https://academind.com/tutorials/reference-vs-primitive-values
+
+### 'this' Keyword & Function References
+
+https://academind.com/tutorials/this-keyword-function-references
---
+++ udemy_courses/NodeJs-The_complet_guide/section_3_understanding_the_basics/_26/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    res.write('<head><title>Page Title</title></head>');
+    res.write('<body><h1>My First Heading</h1><p>My first paragraph.</p></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
+
+
