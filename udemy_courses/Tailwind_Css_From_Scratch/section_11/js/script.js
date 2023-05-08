e03d4473751955816e1a0eef3336a2cdacd76258
Chris Farrugia <foxcode9@gmail.com>
2023-05-08 09:58:55 +0200

    complete tailwind course

--- udemy_courses/Tailwind_Css_From_Scratch/section_11/js/script.js
+++ udemy_courses/Tailwind_Css_From_Scratch/section_11/js/script.js
@@ -4,42 +4,50 @@ const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const logo = document.getElementById('logo')
 
-// Tabs menu event listener
+// -- Tabs menu event listener -----------------------------------------
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+function onTabClick(e) {
+	// - Deactivate all tabs
+	tabs.forEach((tab) => {
+		tab.children[0].classList.remove(
+			'border-softRed', 'border-b-4', 'md:border-b-0'
+		)
+	})
+
+	// - Hide all panels
+	panels.forEach((panel) => panel.classList.add('hidden'))
+
+	// - Activate a new tab and panel based on the target
+	e.target.classList.add('border-softRed', 'border-b-4');
+
+	const classString = e.target.getAttribute('data-target');
+
+	document
+		.getElementById('panels')
+		.getElementsByClassName(classString)[0]
+		.classList.remove('hidden');
+}
+
+// ---------------------------------------------------------------------
+
 
 // Hamburger button listener
 btn.addEventListener('click', navToggle)
 
-function onTabClick(e) {
-  // Deactivate all tabs
-  tabs.forEach((tab) => {
-    tab.children[0].classList.remove(
-      'border-softRed',
-      'border-b-4',
-      'md:border-b-0'
-    )
-  })
-
-  // Hide all panels
-  panels.forEach((panel) => panel.classList.add('hidden'))
-
-  // Activate a new tab and panel based on the target
-  e.target.classList.add('border-softRed', 'border-b-4')
-  const classString = e.target.getAttribute('data-target')
-  document
-    .getElementById('panels')
-    .getElementsByClassName(classString)[0]
-    .classList.remove('hidden')
-}
-
 function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
-  menu.classList.toggle('hidden')
+	btn.classList.toggle('open')
+	// menu.classList.toggle('flex')
+	// menu.classList.toggle('hidden')
+	menu.classList.toggle('invisible')
+	menu.classList.toggle('visible')
+	menu.classList.toggle('opacity-0')
+	menu.classList.toggle('opacity-90')
+	
 
-  if (menu.classList.contains('flex')) {
-    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
-  } else {
-    logo.setAttribute('src', './images/logo-bookmark.svg')
-  }
+	if (menu.classList.contains('visible')) {
+		logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+	} else {
+		logo.setAttribute('src', './images/logo-bookmark.svg')
+	}
 }
