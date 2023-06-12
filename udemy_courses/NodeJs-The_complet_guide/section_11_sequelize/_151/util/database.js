647efbb39fea1a0337970976c94f07ac1030741e
foxcodenine <foxcode9@gmail.com>
2023-06-12 21:28:24 +0200

    node course -starting sequelize

---
+++ udemy_courses/NodeJs-The_complet_guide/section_11_sequelize/_151/util/database.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const envAbsolutPath = path.join(__dirname,  '../.env');
+dotenv.config({ path: envAbsolutPath });
+// ---------------------------------------------------------------------
+
+const sequelize = new Sequelize(
+        process.env.DATABASE_NAME,
+        process.env.DATABASE_USER,
+        process.env.DATABASE_PASSWORD,
+        {
+            dialect: 'mysql',
+            host: process.env.DATABASE_HOST
+        }
+    )
+
+module.exports = sequelize;
