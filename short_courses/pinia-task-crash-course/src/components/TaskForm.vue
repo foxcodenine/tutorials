8e92ef8f083d9e10e50eb07b47ce9f276e2aea10
Christopher Farrugia <foxcode9@gmail.com>
2023-06-21 19:36:02 +0200

    start and complete a pinia crash course

---
+++ short_courses/pinia-task-crash-course/src/components/TaskForm.vue
@@ -0,0 +1,43 @@
+<template >
+    <form @submit.prevent="handleSubmit">
+        <input type="text" placeholder="I need to..." v-model="newTask">
+        <button type="submit">Add</button>
+    </form>
+</template>
+
+<!-- --------------------------------------------------------------- -->
+
+<script>
+import { useTaskStore } from '../stores/TaskStore';
+import { v4 as uuidv4 } from 'uuid';
+export default {
+    setup() {
+        const taskStore = useTaskStore();
+        return { taskStore }
+    },
+    data() {
+        return {
+            newTask: '',
+        }
+    },
+    methods: {
+        handleSubmit() {
+            if (this.newTask.length <= 0) return;
+
+            this.taskStore.addTask({
+                title: this.newTask,
+                isFave: false,
+                id: uuidv4()
+            });
+
+            this.newTask = '';
+        }
+    }
+}
+</script>
+
+<!-- --------------------------------------------------------------- -->
+
+<style lang="sass" >
+    
+</style>
