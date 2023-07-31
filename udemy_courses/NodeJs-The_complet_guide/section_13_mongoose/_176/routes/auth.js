a61dae951ad8f2be0529223ef1027f75f58695dc
foxcodenine <foxcode9@gmail.com>
2023-07-31 18:16:55 +0200

    node course - complete chapter 14

---
+++ udemy_courses/NodeJs-The_complet_guide/section_13_mongoose/_176/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const adminController = require('../controllers/authController');
+
+// ---------------------------------------------------------------------
+
+router.get('/login', (req, res, next) => {
+
+})
+
+
+// ---------------------------------------------------------------------
+
+module.exports = router;
