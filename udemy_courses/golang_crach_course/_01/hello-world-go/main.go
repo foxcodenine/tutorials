acc556c03580173470be12a84eba04eb2a0877b4
Chris Farrugia <foxcode9@gmail.com>
2023-09-20 07:23:22 +0200

    start learning golang

---
+++ udemy_courses/golang_crach_course/_01/hello-world-go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var whatToSay string
+	// whatToSay = "Hello, world again!"
+
+	// var whatToSay = "Hello, world again!"
+
+	whatToSay := "Hello, world again!"
+
+	sayHelloWorld(whatToSay)
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
+}
---
+++ udemy_courses/golang_crach_course/_01/hello-world-go/notes.md
@@ -0,0 +1,12 @@
+
+## install update tools
+
+        View->Command_Palette   go install
+                Go:Install/Update Tools
+
+
+
+
+
+        go mod init myapp
+    
