094391b30a6e89cd9490f514a549803c60586d0d
Christopher Farrugia <foxcode9@gmail.com>
2023-10-01 22:33:28 +0200

    go- updating course progress

---
+++ udemy_courses/golang_crach_course/_9/04_create_the_home_page/rps/rps.go
@@ -0,0 +1,50 @@
+package rps
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	ROCK         = 0 // beats scissors. (scissors + 1) % 3 = 0
+	PAPER        = 1 // beats ROCK. (ROCK + 1) % 3 = 1
+	SCISSORS     = 2 // beats PAPER. (PAPER + 1) % 3 = 2
+	PLAYER_WINS  = 1
+	COMPUTER_WIN = 2
+	DRAW         = 3
+)
+
+// ---------------------------------------------------------------------
+
+func PlayRound(playerValue int) (int, string, string) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// -- move computer choice in 'for' loop, to reset computerValue each loop
+	computerValue := r.Intn(3)
+	computerChoice := ""
+	roundResult := ""
+	winner := 0
+
+	switch computerValue {
+	case ROCK:
+		computerChoice = "Computer chose ROCK"
+	case PAPER:
+		computerChoice = "Computer chose PAPER"
+	case SCISSORS:
+		computerChoice = "Computer chose SCISSORS"
+	default:
+	}
+
+	if playerValue == computerValue {
+		roundResult = "It's a draw"
+		winner = DRAW
+	} else if playerValue == (computerValue+1)%3 {
+		roundResult = "Player wins!"
+		winner = PLAYER_WINS
+	} else {
+		roundResult = "Computer wins!"
+		winner = COMPUTER_WIN
+	}
+
+	return winner, computerChoice, roundResult
+}
