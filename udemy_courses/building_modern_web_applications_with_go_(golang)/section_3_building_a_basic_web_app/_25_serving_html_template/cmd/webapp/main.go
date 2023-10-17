0694506a00d390efb52a7605fc18dcd52c057d49
foxcodenine <foxcode9@gmail.com>
2023-10-17 20:35:31 +0200

    updating course progress

--- udemy_courses/building_modern_web_applications_with_go_(golang)/section_3_building_a_basic_web_app/_25_serving_html_template/cmd/webapp/main.go
+++ udemy_courses/building_modern_web_applications_with_go_(golang)/section_3_building_a_basic_web_app/_25_serving_html_template/cmd/webapp/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"foxcodenine/building_modern_web_applications_with_go/pkg/config"
 	"foxcodenine/building_modern_web_applications_with_go/pkg/handlers"
+	"foxcodenine/building_modern_web_applications_with_go/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,7 +13,24 @@ const portNumber = ":8080"
 
 // ---------------------------------------------------------------------
 
+// main function, the entry point of the application.
 func main() {
+	// Initializing an instance of the AppConfig struct.
+	var app config.AppConfig
+
+	// Creating a new template cache.
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	// Saving the created template cache to the app's TemplateCache field.
+	app.TemplateCache = tc
+
+	// Initializing the templates by passing the app's configuration.
+	render.NewTemplates(&app)
+
 	// Register handlers for the home and about routes.
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
