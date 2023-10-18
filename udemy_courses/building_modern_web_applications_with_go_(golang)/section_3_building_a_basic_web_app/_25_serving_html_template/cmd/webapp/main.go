fa40694d387e95afb3d9f7d03b053298433ffa5d
Chris Farrugia <foxcode9@gmail.com>
2023-10-18 21:42:39 +0200

    go - update course progress

--- udemy_courses/building_modern_web_applications_with_go_(golang)/section_3_building_a_basic_web_app/_25_serving_html_template/cmd/webapp/main.go
+++ udemy_courses/building_modern_web_applications_with_go_(golang)/section_3_building_a_basic_web_app/_25_serving_html_template/cmd/webapp/main.go
@@ -15,8 +15,14 @@ const portNumber = ":8080"
 
 // main function, the entry point of the application.
 func main() {
+
 	// Initializing an instance of the AppConfig struct.
 	var app config.AppConfig
+
+	// Sets appPointer, a pointer in the config package, to point to this app, which is an instance of AppConfig.
+	config.SetAppPointer(&app)
+
+	// ----------------------------------------------
 
 	// Creating a new template cache.
 	tc, err := render.CreateTemplateCache()
@@ -28,12 +34,22 @@ func main() {
 	// Saving the created template cache to the app's TemplateCache field.
 	app.TemplateCache = tc
 
-	// Initializing the templates by passing the app's configuration.
+	// Sets app, a pointer in the render package, to point to this app, which is an instance of AppConfig.
 	render.NewTemplates(&app)
 
+	// ----------------------------------------------
+
+	// Creating a new repository using the NewRepo function and passing the address app which is an instance of AppConfig.
+	repo := handlers.NewRepo(&app)
+
+	// Setting the created repository for the handlers using the NewHandlers function.
+	handlers.NewHandlers(repo)
+
+	// ----------------------------------------------
+
 	// Register handlers for the home and about routes.
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	// Start the HTTP server and listen on port 'portNumber'.
 	fmt.Println(fmt.Sprintf("Starting application: http://localhost%s", portNumber))
