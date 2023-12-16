21a23bf5ac62b50f5c03122c8beeb75b87f3372e
foxcodenine <foxcode9@gmail.com>
2023-12-16 14:30:51 +0100

    go - complete section 4

--- udemy_courses/go__develop_nodern_fast_and_secure_web_application/notes/02_external_packages.md
+++ udemy_courses/go__develop_nodern_fast_and_secure_web_application/notes/02_external_packages.md
@@ -3,3 +3,25 @@
     https://github.com/joho/godotenv
 
     $ go get github.com/joho/godotenv
+
+
+### go-chi  (router)
+
+    https://github.com/go-chi/chi
+
+    $ go get -u github.com/go-chi/chi/v5
+
+
+### justinas/nosurf  (csrf token)
+
+    https://github.com/justinas/nosurf
+    https://pkg.go.dev/github.com/justinas/nosurf#section-readme
+
+    $ go get github.com/justinas/nosurf
+
+
+### alexedwards/scs  (sections management for go)
+
+    $ go get github.com/alexedwards/scs/v2
+
+    https://github.com/alexedwards/scs
--- udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_4_go_with_the_flow_an_introduction_to_middleware/_37/pkg/handlers/handlers.go
+++ udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_4_go_with_the_flow_an_introduction_to_middleware/_37/pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"foxcode.io/common"
@@ -37,14 +38,32 @@ func SetHandlersRepository(r *Repository) {
 
 // HomeHandler handles requests to the home page
 func (m *Repository) HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	remoteIP := r.RemoteAddr
+
+	// Get the User-Agent from the request headers
+	userAgent := r.Header.Get("User-Agent")
+
+	// Put the remoteIP and userAgent into the session
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	m.App.Session.Put(r.Context(), "user_agent", userAgent)
+
+	// Render the home page template with the provided data
 	render.RenderTemplate(w, "home-page.tmpl", &common.TemplateData{})
 }
 
 // AboutHandler handles requests to the about page
 func (m *Repository) AboutHandler(w http.ResponseWriter, r *http.Request) {
 
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	userAgent := m.App.Session.GetString(r.Context(), "user_agent")
+
+	fmt.Println("Remote IP:", remoteIP)
+	fmt.Println("User Agent:", userAgent)
+
 	sidekickMap := map[string]string{
-		"morty": "Ooh, wee!",
+		"morty":     "Ooh, wee!",
+		"remote_ip": remoteIP,
 	}
 
 	render.RenderTemplate(w, "about-page.tmpl", &common.TemplateData{
