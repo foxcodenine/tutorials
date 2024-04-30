c7a5a61ea7ff812c6f35c0d56fce907be47ed383
Christopher Farrugia <foxcode9@gmail.com>
2024-04-30 20:54:50 +0200

    golang course - complete section 14

--- udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_14_sending_email/_126/models/templatedata.go
+++ udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_14_sending_email/_126/models/templatedata.go
@@ -4,13 +4,14 @@ import "foxcode.io/internal/forms"
 
 // TemplateData is a shared type used by handlers and render packages.
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float64
-	DataMap   map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float64
+	DataMap         map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
