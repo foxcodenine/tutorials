5c64f0389a28691896926d3ed59fd9fa983ca69a
foxcodenine <foxcode9@gmail.com>
2024-05-08 23:20:20 +0200

    golange course complete section

--- udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_14_sending_email/_126/internal/repository/repository.go
+++ udemy_courses/go__develop_nodern_fast_and_secure_web_application/section_14_sending_email/_126/internal/repository/repository.go
@@ -40,4 +40,8 @@ type DatabaseRepo interface {
 	AllRooms() ([]models.Room, error)
 
 	GetRestrictionsForRoomByDate(roomID int, start, end time.Time) ([]models.RoomRestriction, error)
+
+	InsertBlockForRoom(id int, startDate time.Time) error
+
+	DeleteBlockByID(id int) error
 }
