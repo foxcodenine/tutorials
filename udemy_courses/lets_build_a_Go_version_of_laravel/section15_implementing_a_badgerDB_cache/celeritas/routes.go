aeabede69eaa69fe5e272bfd12a46c75a9d5f111
Chris Farrugia <foxcode9@gmail.com>
2024-09-01 21:43:19 +0200

    complete sections 13 14 15 and 16

---
+++ udemy_courses/lets_build_a_Go_version_of_laravel/section15_implementing_a_badgerDB_cache/celeritas/routes.go
@@ -0,0 +1,34 @@
+package celeritas
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// routes sets up the HTTP request multiplexer with necessary middlewares and routes.
+func (c *Celeritas) routes() http.Handler {
+	mux := chi.NewRouter() // Create a new router
+
+	// Middleware to get the real IP address of the client
+	mux.Use(middleware.RealIP)
+
+	// If debug mode is enabled, use the logger middleware
+	if c.Debug {
+		mux.Use(middleware.Logger)
+	}
+
+	// Middleware to recover from panics and return a 500 internal server error
+	mux.Use(middleware.Recoverer)
+
+	// Middleware to load and save session data for each request
+	mux.Use(c.SessionLoad)
+
+	// Add your application routes here
+	// Example: mux.Get("/", homeHandler)
+
+	mux.Use(c.NoSurf)
+
+	return mux // Return the configured router
+}
