78042329beff7d47818b78e8097e143bcbdf923b
Chris Farrugia <foxcode9@gmail.com>
2024-10-26 15:10:44 +0200

    golang course - complete sections 5 6 and 7

---
+++ udemy_courses/golange_courses/working_with_concurrency_in_golang/section_6_final_project_building_a_subscription_service/_43_final_project/cmd/web/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/gob"
+	"final_project/data"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "8088"
+
+func main() {
+	// connect to the database
+	db := initDB()
+
+	// create sessions
+	session := initSession()
+
+	// create loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// create channels
+
+	// create waitgroup
+	wg := sync.WaitGroup{}
+
+	// set up the application config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		Wait:     &wg,
+		Models:   data.New(db),
+	}
+
+	// set up mail
+
+	// listen for signals
+	go app.listenForShutdown()
+
+	// listen for a web connections
+	app.serve()
+}
+
+func (app *Config) serve() {
+	// start http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	app.InfoLog.Println("Starting web server - http://localhost:" + webPort + "/")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panicln("can't connect to database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	count := 0
+
+	dns := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dns)
+		if err != nil {
+			log.Println("postgress not yet ready...")
+		} else {
+			log.Println("connected to database!")
+			return connection
+		}
+
+		if count > 10 {
+			return nil
+		}
+
+		log.Println("backing off for 1 second.")
+		time.Sleep(1 * time.Second)
+		count++
+
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	gob.Register(data.User{})
+
+	// set up session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
+}
+
+func (app *Config) listenForShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	app.shutdown()
+	os.Exit(0)
+}
+
+func (app *Config) shutdown() {
+	// perform any cleanup task
+	app.InfoLog.Println("would run cleanup task...")
+
+	// block until waitgroup is empty
+	app.Wait.Wait()
+
+	app.InfoLog.Println("closing channels and shutting down application...")
+}
