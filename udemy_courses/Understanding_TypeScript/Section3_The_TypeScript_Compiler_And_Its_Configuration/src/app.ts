5c654ded07863fa512141b38d8b776a0566d091d
Chris Farrugia <foxcode9@gmail.com>
2025-04-14 18:39:00 +0200

    TS Course Complete Section3

---
+++ udemy_courses/Understanding_TypeScript/Section3_The_TypeScript_Compiler_And_Its_Configuration/src/app.ts
@@ -0,0 +1,16 @@
+// import fs from 'node:fs';
+
+let userName: string;
+
+userName = 'Max';
+
+console.log(userName);
+
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+
+
+console.log(add(1, 5));
