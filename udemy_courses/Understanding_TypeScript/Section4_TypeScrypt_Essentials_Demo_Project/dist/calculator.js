e2fa90a70dabed17388748278dd4587cfd1d3684
Christopher Farrugia <foxcode9@gmail.com>
2025-04-14 20:00:29 +0200

    update

---
+++ udemy_courses/Understanding_TypeScript/Section4_TypeScrypt_Essentials_Demo_Project/dist/calculator.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calcInvestments(initial, annualContribution, rate, years) {
+    const investmentData = [];
+    let currentBalance = initial;
+    for (let year = 1; year <= years; year++) {
+        // For subsequent years, add the annual contribution.
+        if (year > 1) {
+            currentBalance += annualContribution;
+        }
+        const endingBalance = currentBalance * rate;
+        investmentData.push({
+            year,
+            startingBalance: currentBalance,
+            endingBalance,
+        });
+        // Update current balance for the next year.
+        currentBalance = endingBalance;
+    }
+    return investmentData;
+}
+function printResults(investmentData) {
+    investmentData.forEach(({ year, startingBalance, endingBalance }) => {
+        console.log(`Year ${year}: Start = €${startingBalance.toFixed(2)}, End = €${endingBalance.toFixed(2)}`);
+    });
+}
+const results = calcInvestments(1000, 100, 1.05, 5);
+printResults(results);
+//# sourceMappingURL=calculator.js.map
---
+++ udemy_courses/Understanding_TypeScript/Section4_TypeScrypt_Essentials_Demo_Project/src/calculator.ts
@@ -0,0 +1,44 @@
+type InvestmentYear = {
+    year: number;
+    startingBalance: number;
+    endingBalance: number;
+};
+
+function calcInvestments(initial: number, annualContribution: number, rate: number, years: number) {
+
+
+    const investmentData: InvestmentYear[] = [];
+    let currentBalance = initial;
+
+    for (let year = 1; year <= years; year++) {
+        // For subsequent years, add the annual contribution.
+        if (year > 1) {
+            currentBalance += annualContribution;
+        }
+
+        const endingBalance = currentBalance * rate;
+        investmentData.push({
+            year,
+            startingBalance: currentBalance,
+            endingBalance,
+        });
+
+        // Update current balance for the next year.
+        currentBalance = endingBalance;
+    }
+
+    return investmentData;
+}
+
+
+function printResults(investmentData: InvestmentYear[]) {
+    investmentData.forEach(({ year, startingBalance, endingBalance }) => {
+        console.log(
+            `Year ${year}: Start = €${startingBalance.toFixed(2)}, End = €${endingBalance.toFixed(2)}`
+        );
+    });
+}
+
+const results = calcInvestments(1000, 100, 1.05, 5);
+
+printResults(results);
