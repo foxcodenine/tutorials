3bf2d67bb9c8cfa61fc7dcbc234b0302ee476e1f
Christopher Farrugia <foxcode9@gmail.com>
2025-05-20 16:36:23 +0200

    update repo

---
+++ udemy_courses/Understanding_TypeScript/Section14/182/src/project-model.ts
@@ -0,0 +1,18 @@
+namespace App {
+    // Enum to define project states
+    export enum ProjectStatus {
+        active,
+        finished
+    }
+
+    // Project model class
+    export class Project {
+        constructor(
+            public id: string,
+            public title: string,
+            public description: string,
+            public people: number,
+            public status: ProjectStatus
+        ) { }
+    }
+}
