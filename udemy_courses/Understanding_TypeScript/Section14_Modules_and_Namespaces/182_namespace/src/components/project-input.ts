5e1368c0be56f260387875aadf86464a0d3a044f
Christopher Farrugia <foxcode9@gmail.com>
2025-05-21 16:35:35 +0200

    End of day commit

--- udemy_courses/Understanding_TypeScript/Section14_Modules_and_Namespaces/182_namespace/src/components/project-input.ts
+++ udemy_courses/Understanding_TypeScript/Section14_Modules_and_Namespaces/182_namespace/src/components/project-input.ts
@@ -1,5 +1,10 @@
 /// <reference path="base-component.ts" />
 
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../state/project-state.ts" /> 
+
+
 namespace App {
     // ============================================================
     // SECTION – UI COMPONENT: ProjectInput
---
+++ udemy_courses/Understanding_TypeScript/Section14_Modules_and_Namespaces/187_es6_modules/src/util/validation.ts
@@ -0,0 +1,45 @@
+// ============================================================
+// SECTION – VALIDATION LOGIC
+// ============================================================
+
+// Interface describing a validatable input
+type Validatable = {
+    value: string | number;
+    required: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+};
+
+// Function to validate based on the Validatable rules
+export function validate(validatableInput: Validatable): boolean {
+    let isValid = true;
+    const { value, required, minLength, maxLength, min, max } = validatableInput;
+
+    if (typeof value === 'string') {
+        if (required != null) {
+            isValid = isValid && value.trim().length !== 0;
+        }
+        if (minLength != null) {
+            isValid = isValid && value.trim().length >= minLength;
+        }
+        if (maxLength != null) {
+            isValid = isValid && value.trim().length <= maxLength;
+        }
+    }
+
+    if (typeof value === 'number') {
+        if (required != null) {
+            isValid = isValid && value !== null;
+        }
+        if (min != null) {
+            isValid = isValid && value >= min;
+        }
+        if (max != null) {
+            isValid = isValid && value <= max;
+        }
+    }
+
+    return isValid;
+}
