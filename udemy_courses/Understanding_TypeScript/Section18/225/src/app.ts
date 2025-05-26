974ee7321ea27aba86b7468104243b44c6042f3b
Christopher Farrugia <foxcode9@gmail.com>
2025-05-26 16:34:12 +0200

    update repo

--- udemy_courses/Understanding_TypeScript/Section18/225/src/app.ts
+++ udemy_courses/Understanding_TypeScript/Section18/225/src/app.ts
@@ -1,3 +1,8 @@
+import { GOOGLE_API_KEY } from "./config";
+import axios from "axios";
+
+// ---------------------------------------------------------------------
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
@@ -5,7 +10,7 @@ function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    // send this to Google API
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${enteredAddress}&key=${GOOGLE_API_KEY}`)
 }
 
 form.addEventListener('submit', searchAddressHandler);
---
+++ udemy_courses/Understanding_TypeScript/Section18/225/src/config_copy.ts
@@ -0,0 +1,4 @@
+export const GOOGLE_API_KEY = '';
+
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+// https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
