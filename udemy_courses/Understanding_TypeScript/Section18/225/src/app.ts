61d8a928b5c732c84261d6268e38293d8aca75dd
Chris Farrugia <foxcode9@gmail.com>
2025-05-27 16:35:31 +0200

    update repo

--- udemy_courses/Understanding_TypeScript/Section18/225/src/app.ts
+++ udemy_courses/Understanding_TypeScript/Section18/225/src/app.ts
@@ -6,11 +6,40 @@ import axios from "axios";
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+type GoogleGeocodingResponse = {
+    results: {
+        geometry: {
+            location: {
+                lat:number, lng: number
+            }
+        }
+    }[],
+    status: 'OK' | 'ZERO_RESULTS',
+}
+
+// ---------------------------------------------------------------------
+
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${enteredAddress}&key=${GOOGLE_API_KEY}`)
+    axios.get< GoogleGeocodingResponse  >(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+
+        if (response.data.status != 'OK') {
+            throw new Error('Could not fetch coordinates')
+        }
+        const coordinates = response.data.results[0].geometry.location 
+        console.log(coordinates)
+    })
+    .catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
 }
+
+// ---------------------------------------------------------------------
 
 form.addEventListener('submit', searchAddressHandler);
