32869d9c698f7aeffa59c3b7cacd18fd1e026f82
Christopher Farrugia <foxcode9@gmail.com>
2025-05-28 16:37:24 +0200

    update repo

--- udemy_courses/Understanding_TypeScript/Section17_3rd_party_libraries_and_TS/215/dist/app.js
+++ udemy_courses/Understanding_TypeScript/Section17_3rd_party_libraries_and_TS/215/dist/app.js
@@ -1,6 +1,19 @@
 import _ from 'lodash';
+import fs from 'node:fs';
+import { z } from 'zod';
+// ---------------------------------------------------------------------
 const number = [1, 2, 3, 4, 5, 6];
 // split that into multiple arrays
 const chunkedArr = _.chunk(number, 2);
-console.log(chunkedArr);
+console.log(chunkedArr); // [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]
+// ---------------------------------------------------------------------
+const dataSchema = z.object({
+    title: z.string(),
+    id: z.number(),
+    values: z.array(z.union([z.string(), z.number()]))
+});
+const contentStr = fs.readFileSync('data.json', { encoding: 'utf8', flag: 'r' });
+const content = JSON.parse(contentStr);
+console.log(content);
+dataSchema.parse(content);
 //# sourceMappingURL=app.js.map
