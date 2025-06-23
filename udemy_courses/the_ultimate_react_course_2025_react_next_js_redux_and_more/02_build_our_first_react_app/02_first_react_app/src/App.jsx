3fe2d2fad73989668a574216f5f26b9795abe20b
Christopher Farrugia <foxcode9@gmail.com>
2025-06-23 22:14:25 +0200

    Starting a new udemy course on react

---
+++ udemy_courses/the_ultimate_react_course_2025_react_next_js_redux_and_more/02_build_our_first_react_app/02_first_react_app/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+
+
+export default function App() {
+	const [advice, setAdvice] = useState("Hello World");
+	const [count, setCount] = useState(0);
+
+	async function getAdvice(params) {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json(res)
+		console.log(data.slip.advice);
+
+		setAdvice(data.slip.advice);
+		setCount(c=>c+1);
+	}
+
+	useEffect(function() {
+		getAdvice();
+	}, []);
+
+	return (
+		<div>
+			<h1>{advice}</h1>
+			<button onClick={getAdvice}>Get advice</button>
+			<Message count={count} />
+		</div>
+	);
+}
+
+function Message(props) {
+	return (
+		<p>You have read <strong>{props.count}</strong> pieces of advice</p>
+	)
+}
